f842bfb9253c037738bcc7ace1b4c80503ff72f1
Jay McCarthy <jay.mccarthy@gmail.com>
2010-10-09 08:53:06 -0600

    Single test form and slightly simpler patterns

--- cas-cad-e.rkt
+++ cas-cad-e.rkt
@@ -13,12 +13,12 @@
                    [(f ...) (reverse (syntax->list #'(id ...)))]
                    ; We want the action of each to be in the opposite
                    ; order as well
-                   [(action ...) (reverse (syntax->list #'((begin body ...) ...)))]
+                   [((action ...) ...) (reverse (syntax->list #'((body ...) ...)))]
                    ; Each branch explicitly lists its next case, but
                    ; we use a list so that the final one can have no next
                    ; We drop the first, because the list is (add1 (length #'(id ...)))
                    ; and if we didn't, case would "drop" to themselves
-                   [((next ...) ...) (reverse (cdr (syntax->list #'(((id)) ... ()))))])
+                   [((next ...) ...) (reverse (cdr (syntax->list #'((id) ... ()))))])
       ; Break is handled by an escape continuation
       #'(let/ec escape
           (syntax-parameterize 
@@ -26,7 +26,7 @@
            ; gets renamed to the escape continuation
            ([break (make-rename-transformer #'escape)])
            ; Each branch's functions gets put here
-           (let* ([f (lambda () action next ...)] ...)
+           (let* ([f (lambda () action ... (next) ...)] ...)
              ; We use the real case macro to do the comparisons, so
              ; we have guaranteed compatibility. That's where 'else'
              ; comes from. =)
@@ -34,32 +34,30 @@
 
 (require tests/eli-tester)
 
-(define printed "")
-(define (cas1 v)
-  (set! printed "")
-  (cas-cad-e v
-             [(1) (set! printed (string-append printed "1"))]
-             [(2) (set! printed (string-append printed "2")) (break 2)]
-             [(3) 3]))
-
-(test (cas1 1) => 2
-      printed => "12"
-      (cas1 2) => 2
-      printed => "2"
-      (cas1 3) => 3
-      printed => ""
-      (cas1 4) => (void)
-      printed => "")
-
-(define (cas3 v)
-  (let ([w true])
-    (cas-cad-e v
-               [(0) (set! w false)]
-               [(1) (if w (break 1) (break 0))])))
-
-(test (cas3 0) => 0
-      (cas3 1) => 1)
-
-(test (cas-cad-e 1 [(1) (values 1 2)]) => (values 1 2)
-      (cas-cad-e 1) => (void)
-      (cas-cad-e 1 [(2) 3] [else 4]) => 4)
+(test
+ (local [(define printed "")
+         (define (cas1 v)
+           (set! printed "")
+           (cas-cad-e v
+                      [(1) (set! printed (string-append printed "1"))]
+                      [(2) (set! printed (string-append printed "2")) (break 2)]
+                      [(3) 3]))]
+   (test (cas1 1) => 2
+         printed => "12"
+         (cas1 2) => 2
+         printed => "2"
+         (cas1 3) => 3
+         printed => ""
+         (cas1 4) => (void)
+         printed => ""))
+ (local [(define (cas3 v)
+           (let ([w true])
+             (cas-cad-e v
+                        [(0) (set! w false)]
+                        [(1) (if w (break 1) (break 0))])))]
+   (test (cas3 0) => 0
+         (cas3 1) => 1))
+ 
+ (cas-cad-e 1 [(1) (values 1 2)]) => (values 1 2)
+ (cas-cad-e 1) => (void)
+ (cas-cad-e 1 [(2) 3] [else 4]) => 4)
