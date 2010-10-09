97320ef3e2fd7dfbc6f950480f67b72f22ce6914
Jay McCarthy <jay.mccarthy@gmail.com>
2010-10-08 21:52:36 -0600

    up

---
+++ cas-cad-e.rkt
@@ -0,0 +1,41 @@
+#lang racket
+(require racket/stxparam)
+(define-syntax-parameter break
+  (λ (stx) (raise-syntax-error 'break "Used outside cas-cad-e" stx)))
+(define-syntax-rule (cas-cad-e e [(n ...) code ...] ...)
+  (let ([tmp e] [earlier? #f])
+    (let/ec esc
+      (syntax-parameterize 
+       ([break (make-rename-transformer #'esc)])
+       (when (or earlier? (equal? tmp n) ...)
+         (set! earlier? #t)
+         code ...)
+       ...))))
+
+(require tests/eli-tester)
+
+(define printed "")
+(define (cas1 v)
+  (set! printed "")
+  (cas-cad-e v
+             [(1) (set! printed (string-append printed "1"))]
+             [(2) (set! printed (string-append printed "2")) (break 2)]
+             [(3) 3]))
+
+(test (cas1 1) => 2
+      printed => "12"
+      (cas1 2) => 2
+      printed => "2"
+      (cas1 3) => 3
+      printed => ""
+      (cas1 4) => (void)
+      printed => "")
+
+(define (cas3 v)
+ (let ([w true])
+   (cas-cad-e v
+              [(0) (set! w false)]
+              [(1) (if w (break 1) (break 0))])))
+
+(test (cas3 0) => 0
+      (cas3 1) => 1)
