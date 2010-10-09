86017801e03ae8f5434c35bced85aa070f2b5367
Jay McCarthy <jay.mccarthy@gmail.com>
2010-10-09 08:49:36 -0600

    comments

--- cas-cad-e.rkt
+++ cas-cad-e.rkt
@@ -5,14 +5,31 @@
 (define-syntax cas-cad-e
   (syntax-parser
    [(_ e:expr [opt body:expr ...+] ...)
-    (with-syntax* ([(id ...) (generate-temporaries #'(opt ...))]
+    (with-syntax* (; Generate an identifier for each case
+                   [(id ...) (generate-temporaries #'(opt ...))]
+                   ; But we want them to appear in the let* in the
+                   ; reverse order, because earlier cases only refer
+                   ; to later cases
                    [(f ...) (reverse (syntax->list #'(id ...)))]
+                   ; We want the action of each to be in the opposite
+                   ; order as well
                    [(action ...) (reverse (syntax->list #'((begin body ...) ...)))]
+                   ; Each branch explicitly lists its next case, but
+                   ; we use a list so that the final one can have no next
+                   ; We drop the first, because the list is (add1 (length #'(id ...)))
+                   ; and if we didn't, case would "drop" to themselves
                    [((next ...) ...) (reverse (cdr (syntax->list #'(((id)) ... ()))))])
+      ; Break is handled by an escape continuation
       #'(let/ec escape
           (syntax-parameterize 
+           ; We break hygeine with a pleasant syntax parameter that
+           ; gets renamed to the escape continuation
            ([break (make-rename-transformer #'escape)])
-           (let* ([f (lambda () (void) action next ...)] ...)
+           ; Each branch's functions gets put here
+           (let* ([f (lambda () action next ...)] ...)
+             ; We use the real case macro to do the comparisons, so
+             ; we have guaranteed compatibility. That's where 'else'
+             ; comes from. =)
              (case e [opt (id)] ...)))))]))
 
 (require tests/eli-tester)
