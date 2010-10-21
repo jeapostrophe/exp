a2b51dd9d287273c823cba399efb0e1ddef9671a
Jay McCarthy <jay.mccarthy@gmail.com>
2010-10-21 14:22:08 -0700

    initial and correct

--- behavec.rkt
+++ behavec.rkt
@@ -7,7 +7,7 @@
     (define reply-ch (make-channel))
     (channel-put monitor (vector reply-ch evt))
     (channel-get reply-ch))
-  (define (b-> make-monitor . ctcs)
+  (define (b-> the-monitor the-base-label . ctcs)
     (define-values (dom-ctcs rng-l) (split-at ctcs (sub1 (length ctcs))))
     (define rng-ctc (first rng-l))
     (define how-many-doms (length dom-ctcs))
@@ -26,26 +26,30 @@
        (define rng-proj
          ((contract-projection rng-ctc) b))
        (λ (f)
+         (define proj-label (list (gensym the-base-label) the-base-label))
          (if (first-order? f)
-             (let ()
-               (define-values (this-monitor this-label) (make-monitor))
-               (λ args 
-                 (parameterize ([current-monitors (list* (vector this-monitor this-label) (current-monitors))])
+             (if (monitor-allows? the-monitor (list proj-label 'proj f))
+                 (λ args 
+                   (define this-label (cons (gensym the-base-label) proj-label))         
                    (define args-ctc
                      (map (λ (dom x) (dom x))
                           dom-projs args))
-                   (if (monitor-allows? this-monitor (list this-label 'call args-ctc))
+                   (if (monitor-allows? the-monitor (list this-label 'call args-ctc))
                        (local [(define ans-ctc
                                  (rng-proj 
                                   (apply f args-ctc)))]
-                         (if (monitor-allows? this-monitor (list this-label 'return args-ctc ans-ctc))
+                         (if (monitor-allows? the-monitor (list this-label 'return args-ctc ans-ctc))
                              ans-ctc
                              (raise-blame-error
-                              b f
+                              (blame-swap b) f
                               "monitor disallowed return with args ~e and ans ~e" args-ctc ans-ctc)))
                        (raise-blame-error
-                        (blame-swap b) f
-                        "monitor disallowed called with args ~e" args-ctc)))))
+                        b f
+                        "monitor disallowed called with args ~e" args-ctc)))
+                 (raise-blame-error
+                  b f
+                  "monitor disallowed projection of ~e"
+                  f))
              (raise-blame-error
               b f
               "expected a function of ~a argument(s), got: ~e"
@@ -54,8 +58,8 @@
            current-monitors))
 
 (module sort/c racket
-  (require 'behavec)
-  (define (make-sort-monitor sort-label)
+  (require 'behavec racket/pretty unstable/match)
+  (define (make-sort-monitor)
     (define event-ch (make-channel))
     (thread 
      (λ ()
@@ -63,30 +67,41 @@
          (match-define (vector reply-ch evt) (channel-get event-ch))
          (define okay?
            (match evt
-             #;[(list 'order 'return (list a c) a<=c?)
-                ...]
              ; Order should not return after sort
-             [(list (cons 'order _) 'return _ _)
-              #;(printf "Got evt ~a with evts ~e\n" evt evts)
-              (not (ormap (match-lambda [(list (cons 'sort _) 'return _ _) #t] [else #f]) evts))]
+             [(list (list application proj 'order) 'return _ _)
+              ; Look for a return from sort...
+              (define found-a-bad-thing?
+                (let sort-loop ([evts evts])
+                  (match evts
+                    [(list) #f]
+                    [(list-rest evt evts)
+                     (match evt
+                       [(list (list _ _ 'sort) 'return _ _)
+                        ; Look for a previous return from this projection of order
+                        (let order-loop ([evts evts])
+                          (match evts
+                            [(list) #f]
+                            [(list-rest evt evts)
+                             (match evt
+                               [(list (list _ (== proj) 'order) 'return _ _)
+                                #t]
+                               [_
+                                (order-loop evts)])]))]
+                       [_
+                        (sort-loop evts)])])))
+              (not found-a-bad-thing?)]
              [else
               #t]))
+         #;(unless okay?
+           (pretty-print evts))
          (channel-put reply-ch okay?)
          (loop (list* evt evts)))))
     event-ch)
+  (define the-monitor
+    (make-sort-monitor))
   (define sort/c
-    (b-> (λ ()
-           (define label (cons 'sort (gensym 'sort)))
-           (printf "New sort monitor: ~a\n" label)
-           (values (make-sort-monitor label)
-                   label))
-         (b-> (λ ()
-                (match-define (vector monitor monitor-label)
-                              (first (current-monitors)))
-                (printf "Connecting order to monitor ~a\n" monitor-label)
-                (define label (list* 'order (gensym 'order) monitor-label))
-                (values monitor
-                        label))
+    (b-> the-monitor 'sort
+         (b-> the-monitor 'order
               any/c any/c boolean?)
          (listof any/c)
          (listof any/c)))
@@ -134,20 +149,18 @@
            (prefix-in racket: racket/base)
            (prefix-in good: 'sort)
            (prefix-in bad: 'bad-sort))
-  (define (good:<= a b) (<= a b))
-  (define (bad:<= a b)
-    (if (zero? (random 2))
-        #f
-        (<= a b)))
-  (define l (build-list 10 (λ (i) (random 10))))
+  (define good:<= <=)
+  (define bad:<= >=)
+  (define l (build-list 2 (λ (i) (random 10))))
   (define sorted-l (racket:sort l <=))
   (test
    (good:sort good:<= l) => sorted-l
    (good:sort good:<= l) => sorted-l
-   (good:sort bad:<= l) =error> "disallowed"
+   ;(good:sort bad:<= l) =error> "disallowed"
    
    (bad:sort good:<= l) => sorted-l
    (bad:sort good:<= l) =error> "disallowed"
-   (bad:sort bad:<= l) =error> "disallowed"))
+   ;(bad:sort bad:<= l) =error> "disallowed"
+   ))
 
 (require 'sort-client)
