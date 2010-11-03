0df52dfaf6e909112126ed5b6645c9ee0510f084
Jay McCarthy <jay.mccarthy@gmail.com>
2010-11-03 17:39:29 -0600

    Simpler compiler

--- temporal-ctcs/regexp.rkt
+++ temporal-ctcs/regexp.rkt
@@ -6,102 +6,48 @@
 (define-syntax (seq stx) (raise-syntax-error 'seq "Outside regex" stx))
 (define-syntax (union stx) (raise-syntax-error 'union "Outside regex" stx))
 
-(define-for-syntax (compile-regex e)
+(define-for-syntax (compile-regex start e end)
   (syntax-parse
    e
    #:literals (seq union *)
    [(* lhs:expr)
     (with-syntax*
-        ([start_* (generate-temporary 'start_*)]
-         [(_ start_l (end_l ...)
-             [state_l ([input_l (next-state_l ...)]
-                       ...)]
-             ...)
-          (compile-regex #'lhs)])
-      (syntax/loc e
-        (nfa start_* (start_*)
-             [start_* ([epsilon (start_l)])]
-             
-             [state_l ([input_l (next-state_l ...)]
-                        ...)]
-             ...
-             [end_l ([epsilon (start_*)])]
-             ...)))]
+        ([start_lhs (generate-temporary 'start_lhs)])
+      (quasisyntax/loc e
+        ([#,start ([epsilon (start_lhs #,end)])]
+         #,@(compile-regex #'start_lhs #'lhs start))))]
    [(seq lhs:expr rhs:expr)
     (with-syntax*
-        ([start_seq (generate-temporary 'start_seq)]
-         [end_seq (generate-temporary 'end_seq)]
-         [(_ start_l (end_l ...)
-             [state_l ([input_l (next-state_l ...)]
-                       ...)]
-             ...)
-          (compile-regex #'lhs)]
-         [(_ start_r (end_r ...)
-             [state_r ([input_r (next-state_r ...)]
-                       ...)]
-             ...)
-          (compile-regex #'rhs)])
-      (syntax/loc e
-        (nfa start_seq (end_seq)
-             [start_seq ([epsilon (start_l)])]
-             
-             
-             [state_l ([input_l (next-state_l ...)]
-                        ...)]
-             ...
-             [end_l ([epsilon (start_r)])]
-             ...
-             [state_r ([input_r (next-state_r ...)]
-                       ...)]
-             ...
-             [end_r ([epsilon (end_seq)])]
-             ...)))]
+        ([start_rhs (generate-temporary 'start_rhs)])
+      (quasisyntax/loc e
+        (#,@(compile-regex start #'lhs #'start_rhs)
+         #,@(compile-regex #'start_rhs #'rhs end))))]
    [(seq lhs:expr rest:expr ...)
-    (compile-regex #'(seq lhs (seq rest ...)))]
+    (compile-regex start #'(seq lhs (seq rest ...)) end)]
    [(union lhs:expr rhs:expr)
     (with-syntax*
-        ([start_union (generate-temporary 'start_union)]
-         [end_union (generate-temporary 'end_union)]
-         [(_ start_l (end_l ...)
-             [state_l ([input_l (next-state_l ...)]
-                       ...)]
-             ...)
-          (compile-regex #'lhs)]
-         [(_ start_r (end_r ...)
-             [state_r ([input_r (next-state_r ...)]
-                       ...)]
-             ...)
-          (compile-regex #'rhs)])
-      (syntax/loc e
-        (nfa start_union (end_union)
-             [start_union ([epsilon (start_l start_r)])]
-             
-             [state_l ([input_l (next-state_l ...)]
-                        ...)]
-             ...
-             [state_r ([input_r (next-state_r ...)]
-                       ...)]
-             ...
-             
-             [end_l ([epsilon (end_union)])]
-             ...
-             [end_r ([epsilon (end_union)])]
-             ...)))]
+        ([start_lhs (generate-temporary 'start_lhs)]
+         [start_rhs (generate-temporary 'start_rhs)])
+      (quasisyntax/loc e
+        ([#,start ([epsilon (start_lhs start_rhs)])]
+         #,@(compile-regex #'start_lhs #'lhs end)
+         #,@(compile-regex #'start_rhs #'rhs end))))]
    [(union lhs:expr rest:expr ...)
-    (compile-regex #'(union lhs (union rest ...)))]
+    (compile-regex start #'(union lhs (union rest ...)) end)]
    [pat:expr
-    (with-syntax*
-        ([start_pat (generate-temporary 'start_pat)]
-         [end_pat (generate-temporary 'end_pat)])
-      (syntax/loc e
-        (nfa start_pat (end_pat)
-             [start_pat ([pat (end_pat)])])))]))
+    (quasisyntax/loc e
+      ([#,start ([pat (#,end)])]))]))
 
 (define-syntax (regex stx)
   (syntax-parse
    stx
    [(_ e:expr)
-    (compile-regex #'e)]))
+    (with-syntax*
+        ([start (generate-temporary 'start)]
+         [end (generate-temporary 'end)])
+      (quasisyntax/loc stx
+        (nfa start (end)
+             #,@(compile-regex #'start #'e #'end))))]))
 
 (define regex-accepts? nfa-accepts?)  
 
