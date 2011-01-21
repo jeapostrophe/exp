e3a573fbed205c03977bab1a6600c6b145c9f9ab
Jay McCarthy <jay.mccarthy@gmail.com>
2011-01-21 11:31:31 -0700

    runs

--- foo/foo.rkt
+++ foo/foo.rkt
@@ -1,3 +1,4 @@
+#lang racket/base
 ; 	Purely-functional Object-Oriented System in Scheme
 ;
 ; The present code implements a classless, delegation-based OO system, similar
@@ -34,42 +35,42 @@
 ; $Id: pure-oo-system.scm,v 1.2 2000/03/01 02:50:40 oleg Exp oleg $
 
 
-		; A functional substitution in a assoc list
+; A functional substitution in a assoc list
 (define (new-mmap mmap tag new-body)
   (cond
-   ((null? mmap) '())
-   ((eq? tag (caar mmap))	; replacement
-    (cons (cons tag new-body) (cdr mmap)))
-   (else (cons (car mmap) (new-mmap (cdr mmap) tag new-body)))))
+    ((null? mmap) '())
+    ((eq? tag (caar mmap))	; replacement
+     (cons (cons tag new-body) (cdr mmap)))
+    (else (cons (car mmap) (new-mmap (cdr mmap) tag new-body)))))
 
 
-		; This function makes a new dispatcher closure -- a new
-		; object: a dispatcher _is_ an object.
-		; A message map is a list of associations of message
-		; selectors with message handlers. A message selector
-		; is a symbol. A message handler is a procedure. It should
-		; take the dispatcher (i.e., _self_) as the first argument,
-		; and message's arguments as other parameters, if any.
-		; Every object always accepts a message 'mmap and replies with
-		; its message map. This feature is used to create an
-		; object with a new state, and to implement a delegation-based
-		; inheritance (see make-point-3D below). The similarity
-		; between the two runs deeper: an object with a changed state
-		; is in a sense a "child" of the original object.
+; This function makes a new dispatcher closure -- a new
+; object: a dispatcher _is_ an object.
+; A message map is a list of associations of message
+; selectors with message handlers. A message selector
+; is a symbol. A message handler is a procedure. It should
+; take the dispatcher (i.e., _self_) as the first argument,
+; and message's arguments as other parameters, if any.
+; Every object always accepts a message 'mmap and replies with
+; its message map. This feature is used to create an
+; object with a new state, and to implement a delegation-based
+; inheritance (see make-point-3D below). The similarity
+; between the two runs deeper: an object with a changed state
+; is in a sense a "child" of the original object.
 (define (make-dispatcher message-map)
   (define (dispatcher selector . args)
     (cond
       ((eq? selector 'mmap) message-map)
       ((assq selector message-map) =>
-        (lambda (handler-ass)
-          (apply (cdr handler-ass) (cons dispatcher args))))
+                                   (lambda (handler-ass)
+                                     (apply (cdr handler-ass) (cons dispatcher args))))
       ((eq? selector 'my-class) (list dispatcher "UNKNOWN")) ; if wasn't defined
       (else						     ; in message-map
-        (error (dispatcher 'my-class) " does not understand " selector))))
+       (error (dispatcher 'my-class) " does not understand " selector))))
   dispatcher)
 
 
-		; point-2D object and its constructor
+; point-2D object and its constructor
 
 (define (make-point-2D x y)
   (define (my-identity) message-map)
@@ -77,32 +78,32 @@
     `((get-x . ,(lambda (self) (list self x)))
       (get-y . ,(lambda (self) (list self y)))
       (set-x . ,(lambda (self new-x)
-		  (list
-		   (make-dispatcher
-		    (new-mmap (self 'mmap) 'get-x
-			      (lambda (self) (list self new-x)))))))
-
+                  (list
+                   (make-dispatcher
+                    (new-mmap (self 'mmap) 'get-x
+                              (lambda (self) (list self new-x)))))))
+      
       (set-y . ,(lambda (self new-y)
-		  (list
-		   (make-dispatcher
-		    (new-mmap (self 'mmap) 'get-y
-			      (lambda (self) (list self new-y)))))))
+                  (list
+                   (make-dispatcher
+                    (new-mmap (self 'mmap) 'get-y
+                              (lambda (self) (list self new-y)))))))
       (identity . ,(lambda (self) (list self my-identity)))
       (my-class . ,(lambda (self) (list self "point-2D")))
       (of-class . ,(lambda (self) (self 'my-class))) ; an example of
-			; sending a message to myself
+      ; sending a message to myself
       ))
   (make-dispatcher message-map))
 
 (define (print-x-y obj)
   (define (rev-apply lst handler) (apply handler lst))
   (rev-apply (obj 'get-x)
-	     (lambda (obj x)
-	       (rev-apply (obj 'get-y)
-			  (lambda (obj y)
-			    (rev-apply (obj 'my-class)
-				       (lambda (obj my-class)
-					 (list my-class x y))))))))
+             (lambda (obj x)
+               (rev-apply (obj 'get-y)
+                          (lambda (obj y)
+                            (rev-apply (obj 'my-class)
+                                       (lambda (obj my-class)
+                                         (list my-class x y))))))))
 
 (define p (make-point-2D 5 6))
 (define p1 (make-point-2D 5 6))
@@ -139,15 +140,15 @@
     (append
      `((get-z . ,(lambda (self) (list self z)))
        (set-z . ,(lambda (self new-z)
-		   (list
-		    (make-dispatcher
-		     (new-mmap (self 'mmap) 'get-z
-			      (lambda (self) (list self new-z)))))))
+                   (list
+                    (make-dispatcher
+                     (new-mmap (self 'mmap) 'get-z
+                               (lambda (self) (list self new-z)))))))
        (my-class . ,(lambda (self) (list self "point-3D")))
-      )
+       )
      ((make-point-2D x y) 'mmap)))	; the superclass
-
-   (make-dispatcher message-map))
+  
+  (make-dispatcher message-map))
 
 
 (define q (make-point-3D 1 2 3))
