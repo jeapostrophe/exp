5ea5f3a55a40b21ed54468d8e3bf503feb348031
Jay McCarthy <jay.mccarthy@gmail.com>
2011-03-14 11:27:12 -0600

    up

--- persona/p3p-db.rkt
+++ persona/p3p-db.rkt
@@ -4,6 +4,10 @@
 
 (define-runtime-path db-pth "p3p-db.rktd")
 
+(define total 0)
+(define unfused 0)
+(define unfused/level 0)
+
 (struct persona (arcana name base-lvl cost) #:transparent)
 (define db
   (with-input-from-file
@@ -17,54 +21,63 @@
         (for/fold ([db db])
           ([a (in-list ps)])
           (match-define (list name base-lvl cost) a)
-          (cons (persona arcana name base-lvl
-                         (cond
-                           [(base-lvl . > . max-lvl)
-                            +inf.0]
-                           [(member name inv)
-                            0]
-                           [else
-                            cost]))
-                db))))))
+          (set! total (add1 total))
+          (unless cost
+            (when (base-lvl . <= . max-lvl)
+              (set! unfused/level (add1 unfused/level)))
+            (set! unfused (add1 unfused)))
+          (if (base-lvl . > . max-lvl)
+              db
+              (cons (persona arcana name base-lvl cost)
+                    db)))))))
 
-(define-runtime-path fusion-db-pth "normal-spread.dat")
+(printf "~a/~a (~a)\n" 
+        (- total unfused) total
+        unfused/level)
+
 (define (trim s) (regexp-replace #px"\\s+$" s ""))
-(define fusion-db (make-hash))
-(with-input-from-file
-    fusion-db-pth
-  (λ ()
-    (for/fold ([reading? #f]
-               [arcana #f]
-               [combos empty])
-      ([l (in-lines)])
-      (match l
-        ["|=======================================|"
-         (values #t #f empty)]
-        [" ---------------------------------------"
-         (hash-set! fusion-db arcana combos)
-         (values #f #f empty)]
-        [_
-         (if
-          reading?
-          (match l
-            [(regexp #px"^\\|\\s+(.+)\\|\\|\\s+(.+)\\s+\\|\\s+(.+)\\s+\\|$"
-                     (list _
-                           (app trim note)
-                           (app trim (and 1st (not "")))
-                           (app trim (and 2nd (not "")))))
-             (values reading?
-                     (match note
-                       [(or (regexp #rx"\\[.+\\]" (list _))
-                            (regexp #rx"-.+-" (list _))
-                            "")
-                        arcana]
-                       [_
-                        note])
-                     (list* (cons 1st 2nd) combos))]
-            [_
-             (values reading? arcana combos)])
-          (values reading? arcana combos))]))
-    (void)))
+(define (parse-db db)
+  (for/fold ([reading? #f]
+             [arcana #f]
+             [combos empty])
+    ([l (in-lines)])
+    (match l
+      ["|=======================================|"
+       (values #t #f empty)]
+      [" ---------------------------------------"
+       (hash-set! db arcana combos)
+       (values #f #f empty)]
+      [_
+       (if
+        reading?
+        (match l
+          [(regexp #px"^\\|\\s+(.+)\\|\\|\\s+(.+)\\s+\\|\\s+(.+)\\s+\\|$"
+                   (list _
+                         (app trim note)
+                         (app trim (and 1st (not "")))
+                         (app trim (and 2nd (not "")))))
+           (values reading?
+                   (match note
+                     [(or (regexp #rx"\\[.+\\]" (list _))
+                          (regexp #rx"-.+-" (list _))
+                          "")
+                      arcana]
+                     [_
+                      note])
+                   (list* (cons 1st 2nd) combos))]
+          [_
+           (values reading? arcana combos)])
+        (values reading? arcana combos))]))
+  (void))
+
+(define-syntax-rule (define-db id pth)
+  (begin
+    (define-runtime-path db-pth pth)
+    (define id (make-hash))
+    (with-input-from-file db-pth (λ () (parse-db id)))))
+
+(define-db normal-db "normal-spread.dat")
+(define-db triangle-db "triangle-spread.dat")
 
 (define arcana->personas (make-hash))
 (define (arcana-personas a)
@@ -76,7 +89,7 @@
                         db)
                 < #:key persona-base-lvl))))
 
-(define (fuse-persona-level 1st 2nd)
+(define (normal-fusion-lvl 1st 2nd)
   (add1
    (floor
     (/ (+ (persona-base-lvl 1st)
@@ -93,38 +106,51 @@
       (+ a b)
       +inf.0))
 
-(struct pairing (1st 2nd cost) #:transparent)
+(struct fusion (cost) #:transparent)
+(struct fusion:normal fusion (1st 2nd) #:transparent)
 
-(define (find-normal-fusion p)
-  (match-define (persona arcana name base-lvl cost) p)
-  (define os
-    (for/fold ([os empty])
-      ([1*2 (in-list (hash-ref fusion-db arcana))])
-      (match-define (cons 1st-arcana 2nd-arcana) 1*2)
-      (for*/fold ([os os])
-        ([1st (arcana-personas 1st-arcana)]
-         [2nd (arcana-personas 2nd-arcana)]
-         #:when (not (equal? 1st 2nd)))
-        (if (equal?
-             p
-             (find-result-persona
-              (fuse-persona-level 1st 2nd)
-              arcana))
-            (cons (pairing 1st 2nd
-                           (+* (persona-cost 1st)
-                               (persona-cost 2nd)))
-                  os)
-            os))))
-  (if (empty? os)
-      #f
-      (first
-       (sort os <
-             #:key pairing-cost))))
+(define (normal-fusions p)
+  (match-define (persona a _ _ _) p)
+  (for/fold ([os empty])
+    ([1*2 (in-list (hash-ref normal-db a))])
+    (match-define (cons 1st-arcana 2nd-arcana) 1*2)
+    (for*/fold ([os os])
+      ([1st (arcana-personas 1st-arcana)]
+       [2nd (arcana-personas 2nd-arcana)]
+       #:when (not (equal? 1st 2nd)))
+      (define cost
+        (+* (persona-cost 1st)
+            (persona-cost 2nd)))
+      (if (and 
+           (equal?
+            p
+            (find-result-persona (normal-fusion-lvl 1st 2nd) a))
+           (not (equal? cost +inf.0)))
+          (cons (fusion:normal cost 1st 2nd) os)
+          os))))
 
+(for ([p (in-list db)]
+      #:when (not (persona-cost p)))
+  (match-define (persona a name lvl _) p)
+  (printf "Lvl ~a. ~a\n" lvl name)
+  
+  (for* ([v
+          (in-list
+           (sort (normal-fusions p) < #:key fusion-cost))])
+    (match v
+      [(fusion:normal cost 1st 2nd)
+       (printf "\t~a x ~a [~a]\n" (persona-name 1st) (persona-name 2nd) cost)]
+      [#f
+       (void)]))
+  
+  
+  )
+
+#|
 (define all
   (for/list ([p (in-list db)]
              #:when (not (persona-cost p)))
-    (cons p (find-normal-fusion p))))
+    (cons p (find-best-fusion/normal p))))
 
 (define all-avail
   (filter cdr all))
@@ -132,8 +158,10 @@
 (define all-avail/sorted
   (sort all-avail < #:key (compose pairing-cost cdr)))
 
-(for ([p*p (in-list all-avail/sorted)]
-      [i (in-range 1 6)])
+(define (persona/name n)
+  (findf (compose (curry string=? n) persona-name) db))
+
+(for ([p*p (in-list all-avail/sorted)])
   (match-define 
    (cons 
     (persona t_arcana t_name t_base-lvl t_cost)
@@ -142,6 +170,9 @@
      (persona 2_arcana 2_name 2_base-lvl 2_cost)
      cost))
    p*p)
-  (printf "~a. ~a = ~a x ~a (~a)\n"
-          i t_name 1_name 2_name
+  (printf "~a = ~a (~a) x ~a (~a) [~a]\n"
+          t_name 
+          1_name 1_cost
+          2_name 2_cost
           cost))
+|#
