68d99051a2bbfa9ff2f53e2966e68235ceaddd03
Jay McCarthy <jay.mccarthy@gmail.com>
2011-06-24 12:04:08 -0600

    Moving to core

--- temporal-ctcs/tests/temp-c/ex-matthias-b.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require "ex-matthias-a.rkt")
-
-(define memory (new memory%))
-
-(define a (send memory malloc))
-(send memory free a) (displayln `(freeing ,a))
-(send memory free a) (displayln `(freeing ,a))
-(send memory free a) (displayln `(freeing ,a))
