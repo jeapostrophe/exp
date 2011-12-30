378f51c1b31edee2953bed7074c08cc4909c6ed8
Jay McCarthy <jay.mccarthy@gmail.com>
2011-12-29 22:00:01 -0700

    Automatic commit at Thu Dec 29 22:00:01 MST 2011

--- .emacs.el
+++ .emacs.el
@@ -940,6 +940,22 @@ given a prefix arg."
 (dolist (hook '(change-log-mode-hook log-edit-mode-hook))
   (add-hook hook (lambda () (flyspell-mode -1))))
 
+;; wanderlust
+(autoload 'wl "wl" "Wanderlust" t)
+(autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
+(autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
+(autoload 'wl-user-agent-compose "wl-draft" nil t)
+
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'wl-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'wl-user-agent
+      'wl-user-agent-compose
+      'wl-draft-send
+      'wl-draft-kill
+      'mail-send-hook))
+
 ;; customs
 
 (custom-set-variables
