f84fa638245d6e9ab076cd53287b2b2f33d18ce7
Jay McCarthy <jay.mccarthy@gmail.com>
2012-01-05 22:00:01 -0700

    Automatic commit at Thu Jan  5 22:00:01 MST 2012

--- bin/start-trayer.sh
+++ bin/start-trayer.sh
@@ -2,4 +2,6 @@
 
 killall trayer
 
+/usr/bin/xcalib /etc/xcalib/profile.icc
+
 exec trayer --edge top --align right --SetDockType true --expand true --width 10 --alpha 0 --tint 0x000000 --height 17 --transparent true
