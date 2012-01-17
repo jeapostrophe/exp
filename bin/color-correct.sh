d1c8e2b411c2456b71ad8df34887485487c8faee
Jay McCarthy <jay.mccarthy@gmail.com>
2012-01-17 09:00:01 -0700

    Automatic commit at Tue Jan 17 09:00:01 MST 2012

---
+++ bin/color-correct.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/bin/xcalib /etc/xcalib/profile.icc
--- bin/start-trayer.sh
+++ bin/start-trayer.sh
@@ -2,6 +2,4 @@
 
 killall trayer
 
-/usr/bin/xcalib /etc/xcalib/profile.icc
-
 exec trayer --edge top --align right --SetDockType true --expand true --width 10 --alpha 0 --tint 0x000000 --height 17 --transparent true
