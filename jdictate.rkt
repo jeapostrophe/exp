9f793c29b6babf3bc18e9335f721fa215b96163b
Jay McCarthy <jay.mccarthy@gmail.com>
2012-01-25 18:00:01 -0700

    Automatic commit at Wed Jan 25 18:00:01 MST 2012

--- jdictate.rkt
+++ jdictate.rkt
@@ -1,2 +1,34 @@
 #lang racket/base
+(require racket/list
+         racket/port
+         net/url)
 
+(define *output-dir* "/tmp/kdict")
+(define (jdict-url start)
+  (url "http" 
+       #f
+       "www.saiga-jp.com"
+       #f #t
+       (list (path/param "cgi-bin" empty)
+             (path/param "dic.cgi" empty))
+       (list (cons 'm "view")
+             (cons 'f "0")
+             (cons 'sc "0")
+             (cons 'j "")
+             (cons 'e "")
+             (cons 'g "")
+             (cons 's "5")
+             (cons 'rt "0")
+             (cons 'start (number->string start))
+             (cons 'sid "1314972880_86353"))
+       #f))
+
+(define (parse u)
+  (printf "get: ~a\n" (url->string u))
+  (display
+   (call/input-url u get-pure-port
+                   port->string
+                   (list (format "Referer: ~a" (url->string u))))))
+
+(printf "got: ~a\n" "http://www.saiga-jp.com/cgi-bin/dic.cgi?m=view&f=0&sc=0&j=&e=&g=&s=5&rt=0&k=&start=51&sid=1314972880_86353")
+(parse (jdict-url 0))
