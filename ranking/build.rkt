b1cf489b3580654a54765931d87b97e82dccd17b
Jay McCarthy <jay.mccarthy@gmail.com>
2012-02-01 10:14:48 -0700

    First real version

--- ranking/build.rkt
+++ ranking/build.rkt
@@ -46,13 +46,12 @@
 
 ;; Sort by ranking
 (require racket/gui)
-(define (id->info id)
-  (node-label (hash-ref id->game id)))
-
-(define (sort-by-ranking games-node ranking-db aspect)
+(define (sort-by-ranking id->game games-node ranking-db aspect)
+  (define (id->info id)
+    (node-label (hash-ref id->game id)))
   (define key (format "Sort~a" aspect))
   (define all-games (node-children games-node))
-  (define games (filter game-completed? all-games))
+  (define completed-games (filter game-completed? all-games))
   (local-require datalog)
   (define ranking-thy (make-theory))
   (datalog!
@@ -100,53 +99,71 @@
       #f]))
 
   (define mentioned? (make-hasheq))
+  (define (mentioned! x y)
+    (unless (= x y)
+            (hash-set! mentioned? x #t)
+            (hash-set! mentioned? y #t)))
   (define (inspect-<= x y)
     (cond
      [(lift-<= x y)
-      (hash-set! mentioned? x #t)
-      (hash-set! mentioned? y #t)
+      (mentioned! x y)
       #t]
      ;; By anti-symmetry
      [(lift-<= y x)
-      (hash-set! mentioned? x #t)
-      (hash-set! mentioned? y #t)
+      (mentioned! x y)
       #f]
      [else
       (unknown-<= x y)]))
-  (define sorted-children
-    (sort (shuffle games) (negate inspect-<=) #:key node-id))
+  (define sorted-completed-games
+    (sort (shuffle completed-games) inspect-<= #:key node-id))
 
-  (define new-children
+  (define id->order (make-hasheq))
+  (for
+   ([i (in-naturals)]
+    [n (in-list sorted-completed-games)])
+   (define id (node-id n))
+   (when (hash-has-key? mentioned? id)
+         (hash-set! id->order id i)))
+
+  (define new-all-games
     (for/list
-     ([i (in-naturals)]
-      [n (in-list sorted-children)])
-     (struct-copy
-      node n
-      [props
-       (hash-set (node-props n)
-                 key
-                 i)])))
+     ([n (in-list all-games)])
+     (define sort-order (hash-ref id->order (node-id n) #f))
+     (if sort-order
+         (struct-copy
+          node n
+          [props
+           (hash-set (node-props n) key sort-order)])
+         n)))
 
   (values ranking-db
-          (struct-copy node games-node [children new-children])))
+          (struct-copy node games-node [children new-all-games])))
 
-(define path "/home/jay/Dev/scm/github.jeapostrophe/home/etc/games.org")
-(match-define (list games ranking meta) (with-input-from-file path read-org))
+(require racket/package)
+(package-begin
+ (define path "/home/jay/Dev/scm/github.jeapostrophe/home/etc/games.org")
+ (match-define (list games ranking meta) (with-input-from-file path read-org))
 
-(define-values (id->game games*) (id-games games))
-(match-define
- (list overall-ranking story-ranking mechanic-ranking)
- (with-input-from-string (apply string-append (node-content ranking))
-   read))
-(define-values (overall-ranking* games**)
-  (sort-by-ranking games* overall-ranking "Overall"))
-(define ranking*
-  (struct-copy node ranking
-               [content
-                (list
-                 (with-output-to-string
-                   (λ ()
-                      (write
-                       (list overall-ranking* story-ranking mechanic-ranking)))))]))
+ ;; XXX Gather more information from GiantBomb (like genre, etc)
+ (define*-values (id->game games) (id-games games))
+ (match-define
+  (list overall-ranking story-ranking mechanic-ranking)
+  (with-input-from-string (apply string-append (node-content ranking))
+    read))
+ (define*-values (overall-ranking games)
+   (sort-by-ranking id->game games overall-ranking "Overall"))
+ (define*-values (story-ranking games)
+   (sort-by-ranking id->game games story-ranking "Story"))
+ (define*-values (mechanic-ranking games)
+   (sort-by-ranking id->game games mechanic-ranking "Mechanic"))
+ ;; XXX Compute the default sort that emacs does
+ (define* ranking
+   (struct-copy node ranking
+                [content
+                 (list
+                  (with-output-to-string
+                    (λ ()
+                       (write
+                        (list overall-ranking story-ranking mechanic-ranking)))))]))
 
-(write-org (list games** ranking* meta))
+ (write-org (list games ranking meta)))
