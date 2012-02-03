bbd66922d552729a054212a8d46b0ae72c0c6370
Jay McCarthy <jay.mccarthy@gmail.com>
2012-02-03 10:00:01 -0700

    Automatic commit at Fri Feb  3 10:00:01 MST 2012

--- bom2se.rkt
+++ bom2se.rkt
@@ -1,24 +1,23 @@
 #lang racket
-(require net/url
+(require racket/package
+         net/url
          (planet neil/html-parsing/html-parsing)
          (planet clements/sxml2))
 
-(define u "http://www.lds.org/scriptures/bofm/1-ne/1?lang=jpn")
-"http://www.lds.org/scriptures/bofm/1-ne/1?lang=en"
+(package-begin
+ (define u "http://www.lds.org/scriptures/bofm/1-ne/1?lang=jpn")
+ (define xe (call/input-url (string->url u) get-pure-port html->xexp))
+ (define (get-div class)
+   (first ((sxpath (format "//div[@class=~s]" class)) xe)))
 
-(define xe (call/input-url (string->url u) get-pure-port html->xexp))
-(define (get-div class)
-  (first ((sxpath (format "//div[@class=~s]" class)) xe)))
+ #;(pretty-print xe)
 
-#;(pretty-print xe)
+ (define subtitle-raw (list-tail (get-div "subtitle") 2))
+ (define intro-raw (list-tail (get-div "intro") 2))
+ (define summary-raw (rest (first (list-tail (get-div "summary") 2))))
+ (define verses-xe (list '*TOP* (get-div "verses")))
 
-(define subtitle (list-tail (get-div "subtitle") 2))
-(define intro (list-tail (get-div "intro") 2))
-(define summary (rest (first (list-tail (get-div "summary") 2))))
-(define verses-xe (list '*TOP* (get-div "verses")))
-
-(define (simpl-verse p)
-  (list-tail
+ (define (simpl-verse p)
    (append-map
     (match-lambda
      [(? string? e)
@@ -29,35 +28,57 @@
       rest]
      [(list-rest (or 'sup 'span) _)
       empty])
-    (list-tail p 2))
-   2))
+    p))
 
-(define (simpl-verse->expression l)
-  (map (match-lambda
-        [(? string? e)
-         e]
-        [`(ruby ,kanji)
-         kanji]
-        [`(ruby (rp "(") (rt ,reading) (rp ")"))
-         reading]
-        [`(ruby ,kanji (rp "(") (rt ,reading) (rp ")"))
-         kanji])
-       l))
-(define (simpl-verse->reading l)
-  (map (match-lambda
-        [(? string? e)
-         e]
-        [`(ruby ,kanji)
-         kanji]
-        [`(ruby (rp "(") (rt ,reading) (rp ")"))
-         reading]
-        [`(ruby ,kanji (rp "(") (rt ,reading) (rp ")"))
-         (format "~a「~a」" kanji reading)])
-       l))
+ (define (simpl-verse->expression l)
+   (filter-map
+    (match-lambda
+     [(? string? e)
+      e]
+     [`(ruby ,kanji)
+      kanji]
+     [`(ruby (rp "(") (rt ,reading) (rp ")"))
+      #f]
+     [`(ruby ,kanji (rp "(") (rt ,reading) (rp ")"))
+      kanji])
+    l))
+ (define (simpl-verse->reading l)
+   (map (match-lambda
+         [(? string? e)
+          e]
+         [`(ruby ,kanji)
+          kanji]
+         [`(ruby (rp "(") (rt ,reading) (rp ")"))
+          (format "「~a」" reading)]
+         [`(ruby ,kanji (rp "(") (rt ,reading) (rp ")"))
+          (format "~a「~a」" kanji reading)])
+        l))
+ (define (los->s los)
+   (regexp-replace*
+    #rx"^ +"
+    (regexp-replace* #rx" +$"
+                     (regexp-replace* #rx"\u200B" (apply string-append los) "")
+                     "")
+    ""))
 
-(define verses (map simpl-verse ((sxpath "//p") verses-xe)))
-(define kanji-verses (map simpl-verse->expression verses))
-(define reading-verses (map simpl-verse->reading verses))
+ (define (prep-verse v)
+   (list-tail v 2))
+ (define (combine-verse sv)
+   (define kanji (simpl-verse->expression sv))
+   (define reading (simpl-verse->reading sv))
+   (cons (los->s kanji)
+         (los->s reading)))
+ (define clip-verse prep-verse)
 
-(pretty-print
- reading-verses)
+ (define parse-verse (compose combine-verse simpl-verse))
+
+ (define subtitle (parse-verse subtitle-raw))
+ (define intro (parse-verse intro-raw))
+ (define summary (parse-verse summary-raw))
+ (define verses (map (compose combine-verse clip-verse simpl-verse prep-verse)
+                     ((sxpath "//p") verses-xe)))
+
+ (void))
+
+"http://www.lds.org/scriptures/bofm/1-ne/1?lang=en"
+
