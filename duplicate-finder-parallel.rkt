182ede859105a13d4d16c765a57df4ea007bfd40
Jay McCarthy <jay.mccarthy@gmail.com>
2012-02-17 16:16:30 -0700

    check

--- duplicate-finder-parallel.rkt
+++ duplicate-finder-parallel.rkt
@@ -5,31 +5,31 @@
 
 (define (find-duplicates directory)
   (printf "Finding in ~a\n" directory)
-  (define file-hash (make-hash))
-  (define place-list
+  (define compute-file-hash
     (for/fold
-     ([ps empty])
+     ([cfh (λ (file-hash) file-hash)])
      ([f (directory-list directory)])
      (define full-path (build-path directory f))
      (cond
       [(directory-exists? full-path)
-       (cons (place-find-duplicates-spawn full-path)
-             ps)]
+       (define p (place-find-duplicates-spawn full-path))
+       (λ (fh)
+          (combine-hash (cfh fh)
+                        (place-channel-get p)))]
       [else
-       (hash-cons! file-hash
-                   ;; I had an idea to first find out the things
-                   ;; that are the same size and then go back and
-                   ;; in parallel check each of their sha1
-                   ;; checksums, but just changing this line only
-                   ;; save 30 ms, so it probably isn't worth it.
-                   #;(file-size full-path)
-                   (call-with-input-file full-path sha1)
-                   full-path)
-       ps])))
-  (for ([p place-list])
-       (combine-hash! file-hash
-                      (place-channel-get p)))
-  file-hash)
+       (define stamp
+         ;; I had an idea to first find out the things
+         ;; that are the same size and then go back and
+         ;; in parallel check each of their sha1
+         ;; checksums, but just changing this line only
+         ;; save 30 ms, so it probably isn't worth it.
+         #;(file-size full-path)
+         (call-with-input-file full-path sha1))
+       (λ (fh)
+          (hash-cons (cfh fh)
+                     stamp
+                     full-path))])))
+  (compute-file-hash (make-hash)))
 
 (define (print-duplicates file-hash)
   (for ([l (hash-values file-hash)]
