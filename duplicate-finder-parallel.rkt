d8d946730c2da801bd915391550b12642d9ad8ee
Jay McCarthy <jay.mccarthy@gmail.com>
2012-02-17 16:07:15 -0700

    check

--- duplicate-finder-parallel.rkt
+++ duplicate-finder-parallel.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require file/md5)
+(require openssl/sha1)
 
 (provide main)
 
@@ -13,26 +13,25 @@
      (define full-path (build-path directory f))
      (cond
       [(directory-exists? full-path)
-       (define p (place ch (place-find-duplicates ch)))
-       (place-channel-put p full-path)
-       (cons p ps)]
+       (cons (place-find-duplicates-spawn full-path)
+             ps)]
       [else
        (add-or-append! file-hash
-                       (md5 (file->bytes full-path))
+                       (call-with-input-file full-path sha1)
                        (list full-path))
        ps])))
   (for ([p place-list])
-       (define assocs (place-channel-get p))
-       (combine-hash! file-hash assocs))
+       (combine-hash! file-hash
+                      (place-channel-get p)))
   file-hash)
 
 (define (print-duplicates file-hash)
-  (for ([l (hash-values file-hash)])
-       (when (> (length l) 1)
-             (printf "duplicates:~n")
-             (for ([p l])
-                  (printf "\t~a~n" p))
-             (newline))))
+  (for ([l (hash-values file-hash)]
+        #:when (> (length l) 1))
+       (printf "duplicates:~n")
+       (for ([p l])
+            (printf "\t~a~n" p))
+       (newline)))
 
 (define (add-or-append! h k val-list)
   (hash-update! h k (curry append val-list) empty))
@@ -41,13 +40,18 @@
   (for ([p assocs])
        (add-or-append! h (first p) (rest p))))
 
+(define (place-find-duplicates-spawn pth)
+  (define p (place ch (place-find-duplicates ch)))
+  (place-channel-put p pth)
+  p)
+
 (define (place-find-duplicates ch)
   (define directory (place-channel-get ch))
   (define file-hash (find-duplicates directory))
   (place-channel-put ch (hash->list file-hash)))
 
 (define (main)
-  (define p (place ch (place-find-duplicates ch)))
-  (place-channel-put p "/home/jay/Downloads/kdict")
-  (print-duplicates (make-hash (place-channel-get p))))
+  (time
+   (define p (place-find-duplicates-spawn "/home/jay/Downloads/kdict"))
+   (print-duplicates (make-hash (place-channel-get p)))))
 
