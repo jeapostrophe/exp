7493718f4d7ef7faccc05c42cb5c048af494a082
Jay McCarthy <jay.mccarthy@gmail.com>
2012-03-07 09:00:01 -0700

    Automatic commit at Wed Mar  7 09:00:01 MST 2012

--- tw.py
+++ tw.py
@@ -1,3 +1,58 @@
+### News source
+class NewsSource(object):
+    def __init__(self):
+
+    def login(config, section):
+        the_app_key = config.get(section, "app_key")
+        the_app_secret = config.get(section, "app_secret")
+
+        if not config.has_option(section, "oauth_token"):
+            if not config.has_option(section, "auth_verifier"):
+                t = self.connect(app_key = the_app_key,
+                                 app_secret = the_app_secret )
+                auth_props = t.get_authentication_tokens()
+                print "Go to this URL: %s" % auth_props['auth_url']
+                print "Then, add the verifier to the config file as the option auth_verifier under section %s" % section
+                config.set(section, "auth_token", auth_props['oauth_token'])
+                config.set(section, "auth_token_secret", auth_props['oauth_token_secret'])
+                save_config()
+                exit(0)
+
+                t = self.connect(
+                    app_key = the_app_key,
+                    app_secret = the_app_secret,
+                    oauth_token = config.get(section, "auth_token"),
+                    oauth_token_secret = config.get(section, "auth_token_secret") )
+                authorized_tokens = t.get_access_token(config.get(section, "auth_verifier"))
+                config.set(section, "auth_token", authorized_tokens['oauth_token'])
+                config.set(section, "auth_token_secret", authorized_tokens['oauth_token_secret'])
+                save_config()
+
+            return self.connect(
+                app_key = the_app_key,
+                app_secret = the_app_secret,
+                oauth_token = config.get(section, "oauth_token"),
+                oauth_token_secret = config.get(section, "oauth_token_secret") )
+
+class TumblrNewsSource(NewsSource):
+    def __init__(self):
+
+    def connect(self, app_key = None, app_secret = None, oauth_token = None, oauth_token_secret = None):
+        return Tumblpy(app_key = app_key,
+                       app_secret = app_secret,
+                       oauth_token = oauth_token,
+                       oauth_token_secret = oauth_token_secret )
+
+class TwitterNewsSource(NewsSource):
+    def __init__(self):
+
+    def connect(self, app_key = None, app_secret = None, oauth_token = None, oauth_token_secret = None):
+        return Twython(twitter_token = app_key,
+                       twitter_secret = app_secret,
+                       oauth_token = oauth_token,
+                       oauth_token_secret = oauth_token_secret )
+
+### Configuration
 import ConfigParser
 from os.path import expanduser
 
@@ -10,51 +65,17 @@ config = ConfigParser.RawConfigParser()
 config.read(config_path)
 
 from twython import Twython
+tw1 = TwitterNewsSource().login(config, "twitter1")
+tw2 = TwitterNewsSource().login(config, "twitter2")
 
-# XXX Multiple twitter accounts
-
-twitter = Twython(
-    twitter_token = config.get("twitter", "app_key"),
-    twitter_secret = config.get("twitter", "app_secret"),
-    oauth_token = config.get("twitter", "oauth_token"),
-    oauth_token_secret = config.get("twitter", "oauth_token_secret"))
-results = twitter.getHomeTimeline(count = 200)
-
+results = tw1.getHomeTimeline(count = 200)
 for t in results:
+    t['id']
     print "@%s: %s (%s)" % (t['user']['screen_name'], t['text'], t['created_at'])
 
 from tumblpy import Tumblpy
-
-the_app_key = config.get("tumblr", "app_key")
-the_app_secret = config.get("tumblr", "app_secret")
-
-if not config.has_option("tumblr", "oauth_token"):
-    if not config.has_option("tumblr", "auth_verifier"):
-        t = Tumblpy(app_key = the_app_key,
-                    app_secret = the_app_secret )
-        auth_props = t.get_authentication_tokens()
-        print "Go to this URL: %s" % auth_props['auth_url']
-        print "Then, add the verifier to the config file as the option auth_verifier"
-        config.set("tumblr", "auth_token", auth_props['oauth_token'])
-        config.set("tumblr", "auth_token_secret", auth_props['oauth_token_secret'])
-        save_config()
-        exit(0)
-
-    t = Tumblpy(app_key = the_app_key,
-                app_secret = the_app_secret,
-                oauth_token = config.get("tumblr", "auth_token"),
-                oauth_token_secret = config.get("tumblr", "auth_token_secret") )
-    authorized_tokens = t.get_access_token(config.get("tumblr", "auth_verifier"))
-    config.set("tumblr", "auth_token", authorized_tokens['oauth_token'])
-    config.set("tumblr", "auth_token_secret", authorized_tokens['oauth_token_secret'])
-    save_config()
-
-t = Tumblpy(app_key = the_app_key,
-            app_secret = the_app_secret,
-            oauth_token = config.get("tumblr", "oauth_token"),
-            oauth_token_secret = config.get("tumblr", "oauth_token_secret") )
-
-print len(t.get('user/dashboard', params = {'offset': 20})['posts'])
+tu = TumblrNewsSource().login(config, "tumblr")
+print len(tu.get('user/dashboard', params = {'offset': 20})['posts'])
 
 # XXX Both:
 # XXXX Remember last entry, get them all since then, produce an rss feed
