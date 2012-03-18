5ec6124d50f4c88460db6796093a8afbebdf3336
Jay McCarthy <jay.mccarthy@gmail.com>
2012-03-17 23:00:01 -0600

    Automatic commit at Sat Mar 17 23:00:01 MDT 2012

--- bin/git-sync.sh
+++ bin/git-sync.sh
@@ -5,7 +5,7 @@ export XAUTHORITY=~/.Xauthority
 
 chpwd () {}
 
-for REPO in ~exp ~home ~work ~github/jpn ; do
+for REPO in ~exp ~home ~work ~github/jpn ~gb ; do
     cd $REPO
     # Remove deleted files
     git ls-files --deleted -z | xargs -0 git rm >/dev/null 2>&1
