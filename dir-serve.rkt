a8ab150038dcaf026918ea9a89dd5fb9ece0aacc
Jay McCarthy <jay.mccarthy@gmail.com>
2012-03-21 08:00:02 -0600

    Automatic commit at Wed Mar 21 08:00:02 MDT 2012

--- dir-serve.rkt
+++ dir-serve.rkt
@@ -4,12 +4,16 @@
          web-server/dispatchers/filesystem-map
          racket/cmdline)
 
+(define the-port 8000)
+
 (command-line
+ #:once-each
+ ["-p" port "the port to use" (set! the-port (string->number port))]
  #:args (base)
  (serve
   #:dispatch
   (files:make #:url->path (make-url->path base))
-  #:port 8000))
+  #:port the-port))
 
 (do-not-return)
 
