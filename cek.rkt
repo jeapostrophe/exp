da2d4cb009e2b938ef68d94f32f5efcbb9c9bb35
Jay McCarthy <jay.mccarthy@gmail.com>
2012-03-28 14:05:52 -0600

    .

--- cek.rkt
+++ cek.rkt
@@ -1,26 +1,42 @@
 #lang racket/base
+(require racket/match)
 
-(struct clo (term env))
+(struct pv () #:transparent)
+(struct add pv () #:transparent)
+(struct num pv (n) #:transparent)
+(struct clo pv (term env) #:transparent)
 
 (define step
   (match-lambda
-   [(vector (? clo? pv) E `(* ,Ep ,e --> ,k))
-    (vector e Ep `(,pv * ,k))]
-   [(vector (? clo? pv) E `(,(clo `(λ (,x) ,e) ,Ep) * --> ,k))
+   [(vector (? pv? pv) E `(* ,Ep ,e --> ,k))
+    (vector e Ep `(,pv * --> ,k))]
+   [(vector (? pv? pv) E 
+            `(,(clo `(λ (,x) ,e) Ep) * --> ,k))
     (vector e (hash-set Ep x pv) k)]
    [(vector (? symbol? x) E k)
     (vector (hash-ref E x) E k)]
    [(vector `(,e ,ep) E k)
     (vector e E `(* ,E ,ep --> ,k))]
-   [(vector `(λ (,x) ,e) ,E ,k)
+   [(vector `(λ (,x) ,e) E k)
     (vector (clo `(λ (,x) ,e) E) E k)]))
 
 (define step*
   (match-lambda
-   [(vector (clo t _) _ 'halt)
-    t]
+   [(vector (? pv? pv) _ 'halt)
+    pv]
    [st
     (step* (step st))]))
 
 (define (eval e)
   (step* (vector e (hasheq) 'halt)))
+
+(eval `(λ (s) (λ (z) z)))
+(eval `(λ (s) (λ (z) (s z))))
+(eval `(λ (n) (λ (m)
+                (λ (s) (λ (z)
+                         ((n s) ((m s) z)))))))
+(eval `(((λ (n) (λ (m)
+                  (λ (s) (λ (z)
+                           ((n s) ((m s) z))))))
+         (λ (s) (λ (z) (s z))))
+        (λ (s) (λ (z) (s z)))))
