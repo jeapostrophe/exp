ab6119d6588a26a0a837cb08dcc4adc15a5dcf57
Jay McCarthy <jay.mccarthy@gmail.com>
2012-06-07 16:00:01 -0600

    Automatic commit at Thu Jun  7 16:00:01 MDT 2012

---
+++ coq/CS142.v
@@ -0,0 +1,17 @@
+Require Import Reals.
+
+(* Assignment 1 *)
+
+Theorem a1_e1 :
+ (( 12 / 8 ) * 3)%R = ( 4 + 1/2 )%R.
+Proof.
+ field.
+Qed.
+
+Theorem a1_e2 :
+ (((50 * 8) + (80 * 1)) / 9)%R = (53 + 1/3)%R.
+Proof.
+ field.
+Qed.
+
+(* Assignment 2 *)
