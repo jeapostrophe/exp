45c74041e3acd14f695cf122093e905be891eac8
Jay McCarthy <jay.mccarthy@gmail.com>
2012-06-22 07:00:01 -0600

    Automatic commit at Fri Jun 22 07:00:01 MDT 2012

--- ds-armor.rkt
+++ ds-armor.rkt
@@ -8,43 +8,63 @@
 
 (define option-row->option
   (match-lambda
-   [(list name durability weight phys str sls prc mag fir lit poise bleed poison curse)
-    (define vals (map string->number (list weight phys)))
+   [(list name durability weight phys str sls prc mag fir lit poise bleed poison curse have)
+    (define vals (map string->number (list weight phys poise)))
     (and (andmap number? vals)
+         (string=? "1" have)
+         #;(not (regexp-match (regexp-quote "+") name))
          (list* name vals))]))
 
 (define options
   (for/list ([p (in-list positions)])
     (define pth (format "/home/jay/Downloads/Dark Souls Armor - ~a.csv" p))
-    (sort
-     (remove-duplicates (cons (list (format "Bare ~a" p) 0.0 0.0)
-                                 (filter-map option-row->option (rest (read-csv-file pth))))
-                           #:key rest)
-     <=
-     #:key third)))
 
-(printf "option structure: ~v\n" (map length options))
-(printf "~v options\n" (apply * (map length options)))
+    (define choices
+      (cons (list (format "Bare ~a" p) 0 0 0)
+            (filter-map option-row->option (read-csv-file pth))))
+
+    (for/fold ([ht (hash)])
+        ([c (in-list choices)])
+      (match-define (list* name weight phys _) c)
+      (hash-update ht weight
+                   (λ (old)
+                     (match-define (list* _ _ old-phys _) old)
+                     (if (> old-phys phys)
+                       old
+                       c))
+                   c))))
+
+(printf "option structure: ~v\n" (map hash-count options))
+(printf "~v options\n" (apply * (map hash-count options)))
 
 (define (branch-and-bound target-weight current-value options)
   (match options
     [(list)
-     current-value]
+     (values current-value empty)]
     [(list* option options)
-     (for/fold ([best-so-far -inf.0])
-         ([choice (in-list option)])
+     (for/fold ([best-so-far -inf.0]
+                [best-so-far-choice #f])
+         ([(weight choice) (in-hash option)]
+          #:when (<= weight target-weight))
        (match-define (list* _ weight phys _) choice)
        (define new-weight (- target-weight weight))
+       (define new-value (+ current-value phys))
+       (define-values (this-value sub-choice) (branch-and-bound new-weight new-value options))
        (cond
-         [(negative? new-weight)
-          best-so-far]
+         [(> this-value best-so-far)
+          (values this-value (list* choice sub-choice))]
          [else
-          (define new-value (+ current-value phys))
-          (define this-value (branch-and-bound new-weight new-value options))
-          (cond
-            [(> this-value best-so-far)
-             this-value]
-            [else
-             best-so-far])]))]))
+          (values best-so-far best-so-far-choice)]))]))
 
-(branch-and-bound (/ 78 2) 0 options)
+(define-values (_ choices) 
+  (branch-and-bound (- (/ 80 2)
+                       #;6 5
+                       3 #;3.5) 0 options))
+
+(printf "\nBest choices:\n")
+(printf "Total ~a\n"
+        (for/fold ([tots (list 0 0 0)])
+            ([c (in-list choices)])
+          (match-define (list* name vals) c)
+          (printf " ~a ~a\n" vals name)
+          (map + tots vals)))
