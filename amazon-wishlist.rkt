5c0be943d840ecdc898b9d30578f0eca737dd75d
Jay McCarthy <jay.mccarthy@gmail.com>
2012-06-25 18:00:01 -0600

    Automatic commit at Mon Jun 25 18:00:01 MDT 2012

--- amazon-wishlist.rkt
+++ amazon-wishlist.rkt
@@ -1,18 +1,10 @@
 #lang racket
-(require xml
-         net/url
-         (planet neil/html-parsing/html-parsing)
-         (planet clements/sxml2))
+(require (planet neil/csv))
 
-(define (wishlist-url page)
-  (format "http://www.amazon.com/registry/wishlist/STV061I2MKRE/ref=cm_wl_sb_o?reveal=unpurchased&filter=all&sort=date-added&layout=compact&x=17&y=8&page=~a" page))
-
-(define u (wishlist-url 1))
-(define xe (call/input-url (string->url u) get-pure-port html->xexp))
-(define items
-  ;; tr/td/span[@class=\"small productTitle\"]
-  ((sxpath "//table[@class=\"compact-items wlrdZeroTable\"]/tbody[@class=\"itemWrapper\"]") xe))
-
-(require racket/pretty)
-(pretty-print
- items)
+(csv-for-each
+ (match-lambda
+  [(list Author Title Price)
+   (printf "** [[http://www.google.com/search?tbm=bks&q=~a by ~a][~a, by ~a]]\n"
+           Title Author
+           Title Author)])
+ (open-input-file "/home/jay/Downloads/wishlist.csv"))
