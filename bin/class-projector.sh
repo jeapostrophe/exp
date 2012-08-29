9842978e40806f7ed602048051dbe04047573a3c
Jay McCarthy <jay.mccarthy@gmail.com>
2012-08-29 14:00:01 -0600

    Automatic commit at Wed Aug 29 14:00:01 MDT 2012

--- bin/class-projector.sh
+++ bin/class-projector.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-1280x1024
-800x600
+xrandr --newmode "1280x1024_59.90"  108.70  1280 1360 1496 1712  1024 1025 1028 1060  -HSync +Vsync
+xrandr --newmode "1280x1024_60.00"  108.88  1280 1360 1496 1712  1024 1025 1028 1060  -HSync +Vsync
+xrandr --newmode "800x600_60.00"  38.22  800 832 912 1024  600 601 604 622  -HSync +Vsync
+xrandr --newmode "800x600_59.90"  38.09  800 832 912 1024  600 601 604 621  -HSync +Vsync
+
+for i in DP-1 DP-2 ; do
+    for j in "1280x1024_59.90" "1280x1024_60.00" "800x600_60.00" "800x600_59.90" ; do
+        xrandr --addmode ${i} ${j}
+    done
+done
