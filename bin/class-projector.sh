9826bbc98a180da3b86e2baf8897a3113eaee118
Jay McCarthy <jay.mccarthy@gmail.com>
2012-08-30 09:00:01 -0600

    Automatic commit at Thu Aug 30 09:00:01 MDT 2012

--- bin/class-projector.sh
+++ bin/class-projector.sh
@@ -2,12 +2,21 @@
 
 # https://wiki.archlinux.org/index.php/Xrandr
 
-xrandr --newmode "1280x1024_59.90"  108.70  1280 1360 1496 1712  1024 1025 1028 1060  -HSync +Vsync
-xrandr --newmode "1280x1024_60.00"  108.88  1280 1360 1496 1712  1024 1025 1028 1060  -HSync +Vsync
-xrandr --newmode "800x600_60.00"  38.22  800 832 912 1024  600 601 604 622  -HSync +Vsync
-xrandr --newmode "800x600_59.90"  38.09  800 832 912 1024  600 601 604 621  -HSync +Vsync
+#xrandr --output DP-1 --newmode "800x600_59.90"   38.00  800 832 912 1024  600 603 607 624 -hsync +vsync
+
+for j in "1280x1024_59.90" "1280x1024_60.00" "800x600_60.00" "800x600_59.90" ; do
+    xrandr --rmmode ${j}
+done
+
 
 for i in DP-1 DP-2 ; do
+    for j in "1280x1024_59.90" "1280x1024_60.00" "800x600_60.00" "800x600_59.90" ; do
+        xrandr --delmode ${i} ${j}
+    done
+    xrandr --output ${i} --newmode "1280x1024_59.90"  108.70  1280 1360 1496 1712  1024 1025 1028 1060  -HSync +Vsync
+    xrandr --output ${i} --newmode "1280x1024_60.00"  108.88  1280 1360 1496 1712  1024 1025 1028 1060  -HSync +Vsync
+    xrandr --output ${i} --newmode "800x600_60.00"  38.22  800 832 912 1024  600 601 604 622  -HSync +Vsync
+    xrandr --output ${i} --newmode "800x600_59.90"  38.09  800 832 912 1024  600 601 604 621  -HSync +Vsync
     for j in "1280x1024_59.90" "1280x1024_60.00" "800x600_60.00" "800x600_59.90" ; do
         xrandr --addmode ${i} ${j}
     done
