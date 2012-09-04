6f272a4e08fde0dee929665748793082cb43718f
Jay McCarthy <jay.mccarthy@gmail.com>
2012-09-04 11:00:01 -0600

    Automatic commit at Tue Sep  4 11:00:01 MDT 2012

--- bin/class-projector.sh
+++ bin/class-projector.sh
@@ -1,37 +1,16 @@
 #!/bin/bash
 
-# https://wiki.archlinux.org/index.php/Xrandr
+nvidia-settings -q CurrentMetaMode
 
-#xrandr --output DP-1 --newmode "800x600_59.90"   38.00  800 832 912 1024  600 603 607 624 -hsync +vsync
+# Did I need to run disper-e first?
 
-MODES=("1440 900 60" "1280 1024 50" "1280 1024 59.9" "1280 1024 60" "800 600 60" "800 600 59.9" "800 600 50")
-OUTPUTS=("DP-1" "DP-2")
+if [ "x$1" = "xon" ] ; then
+	#nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=1400x900, ViewPortOut=1400x900+20+0 }, DFP-1: 1400x1050 { ViewPortIn=1400x900, ViewPortOut=1400x900+0+75 }"
+	nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=640x480, ViewPortOut=640x480+0+0 }, DFP-1: 640x480 { ViewPortIn=640x480, ViewPortOut=640x480+0+0 }"
+else 
+	nvidia-settings --assign CurrentMetaMode="DPY-2: 1440x900 @1440x900"
+fi
 
-for mode in "${MODES[@]}" ; do
-    echo $mode
-    xrandr --rmmode "${mode}"
-    MODELINE=$(gtf $mode | tail -2 | head -1 | cut -f "5-" -d " ")
-    xrandr --newmode "${mode}" ${MODELINE}
+nvidia-settings -q CurrentMetaMode
 
-    for disp in "${OUTPUTS[@]}" ; do
-        sudo xrandr --delmode "${disp}" "${mode}"
-        sudo xrandr --addmode "${disp}" "${mode}"
-    done
-done
-
-exit 1
-
-for i in DP-1 DP-2 ; do
-    for j in "1280x1024_59.90" "1280x1024_60.00" "800x600_60.00" "800x600_59.90" ; do
-        xrandr --delmode ${i} ${j}
-    done
-    for j in "1280x1024_59.90" "1280x1024_60.00" "800x600_60.00" "800x600_59.90" ; do
-        xrandr --addmode ${i} ${j}
-    done
-done
-
-# https://kis.kellogg.northwestern.edu/Pages/ExternalDisplaysLinux.aspx
-
-# xrandr --output DP-2 --auto --output DP-1 --auto --same-as DP-2
-
-xrandr --output DP-2 --mode "1280x1024_60.00" --output DP-1 --mode "1280x1024_59.90" --same-as DP-2
+~/bin/start-trayer.sh 0 &
