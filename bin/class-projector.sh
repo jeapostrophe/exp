dfd405a3bf4f24b51fa9fe7233986bc0e9ed134a
Jay McCarthy <jay.mccarthy@gmail.com>
2012-09-04 12:00:01 -0600

    Automatic commit at Tue Sep  4 12:00:01 MDT 2012

--- bin/class-projector.sh
+++ bin/class-projector.sh
@@ -6,7 +6,8 @@ nvidia-settings -q CurrentMetaMode
 
 if [ "x$1" = "xon" ] ; then
 	#nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=1400x900, ViewPortOut=1400x900+20+0 }, DFP-1: 1400x1050 { ViewPortIn=1400x900, ViewPortOut=1400x900+0+75 }"
-	nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=640x480, ViewPortOut=640x480+0+0 }, DFP-1: 640x480 { ViewPortIn=640x480, ViewPortOut=640x480+0+0 }"
+	nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=640x480, ViewPortOut=640x480+400+210 }, DFP-1: 640x480 { ViewPortIn=640x480, ViewPortOut=640x480+0+0 }"
+	#nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=800x600, ViewPortOut=800x600+0+0 }, DFP-1: 800x600 { ViewPortIn=800x600, ViewPortOut=800x600+0+0 }"
 else 
 	nvidia-settings --assign CurrentMetaMode="DPY-2: 1440x900 @1440x900"
 fi
