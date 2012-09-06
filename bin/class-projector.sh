839da11cbc293ce3bbb51022cf780fc35ff543d5
Jay McCarthy <jay.mccarthy@gmail.com>
2012-09-06 09:00:01 -0600

    Automatic commit at Thu Sep  6 09:00:01 MDT 2012

--- bin/class-projector.sh
+++ bin/class-projector.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+# First run "disper -e" to get the second one turned on
+
+# Projector
+# DFP-2: 1440x900 { ViewPortIn=1400x900, ViewPortOut=1400x900+20+0 }, DFP-1: 1400x1050 { ViewPortIn=1400x900, ViewPortOut=1400x900+0+75 }, DFP-0: NULL;
+
+# Laptop
+# DFP-2: 1440x900, DFP-1: NULL, DFP-0: NULL;
+
+# Desktop
+# DFP-2: NULL, DFP-1: NULL, DFP-0: 1920x1080;
+
 nvidia-settings -q CurrentMetaMode
 
-# Did I need to run disper-e first?
-
 if [ "x$1" = "xon" ] ; then
+    # This is what OS X can do
 	#nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=1400x900, ViewPortOut=1400x900+20+0 }, DFP-1: 1400x1050 { ViewPortIn=1400x900, ViewPortOut=1400x900+0+75 }"
+
+    # This one works and has a tiny box on the laptop and a very small resolution on the screen
 	nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=640x480, ViewPortOut=640x480+400+210 }, DFP-1: 640x480 { ViewPortIn=640x480, ViewPortOut=640x480+0+0 }"
+
 	#nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=800x600, ViewPortOut=800x600+0+0 }, DFP-1: 800x600 { ViewPortIn=800x600, ViewPortOut=800x600+0+0 }"
 else 
 	nvidia-settings --assign CurrentMetaMode="DPY-2: 1440x900 @1440x900"
