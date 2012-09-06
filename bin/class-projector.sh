34ec687a6bd4d566ea047e260494baf9c8a11043
Jay McCarthy <jay.mccarthy@gmail.com>
2012-09-06 10:00:01 -0600

    Automatic commit at Thu Sep  6 10:00:01 MDT 2012

--- bin/class-projector.sh
+++ bin/class-projector.sh
@@ -20,7 +20,6 @@ if [ "x$1" = "xon" ] ; then
     # This one works and has a tiny box on the laptop and a very small resolution on the screen
 	nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=640x480, ViewPortOut=640x480+400+210 }, DFP-1: 640x480 { ViewPortIn=640x480, ViewPortOut=640x480+0+0 }"
 
-	#nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=800x600, ViewPortOut=800x600+0+0 }, DFP-1: 800x600 { ViewPortIn=800x600, ViewPortOut=800x600+0+0 }"
 else 
 	nvidia-settings --assign CurrentMetaMode="DPY-2: 1440x900 @1440x900"
 fi
