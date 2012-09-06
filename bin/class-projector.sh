cec7991c1ad4fa3c0ba7a146c94934b2485769f9
Jay McCarthy <jay.mccarthy@gmail.com>
2012-09-06 17:00:01 -0600

    Automatic commit at Thu Sep  6 17:00:01 MDT 2012

--- bin/class-projector.sh
+++ bin/class-projector.sh
@@ -15,10 +15,10 @@ nvidia-settings -q CurrentMetaMode
 
 if [ "x$1" = "xon" ] ; then
     # This is what OS X can do
-	#nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=1400x900, ViewPortOut=1400x900+20+0 }, DFP-1: 1400x1050 { ViewPortIn=1400x900, ViewPortOut=1400x900+0+75 }"
+	nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=1400x900, ViewPortOut=1400x900+20+0 }, DFP-1: 1400x1050 { ViewPortIn=1400x900, ViewPortOut=1400x900+0+75 }"
 
     # This one works and has a tiny box on the laptop and a very small resolution on the screen
-	nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=640x480, ViewPortOut=640x480+400+210 }, DFP-1: 640x480 { ViewPortIn=640x480, ViewPortOut=640x480+0+0 }"
+	#nvidia-settings --assign CurrentMetaMode="DFP-2: 1440x900 { ViewPortIn=640x480, ViewPortOut=640x480+400+210 }, DFP-1: 640x480 { ViewPortIn=640x480, ViewPortOut=640x480+0+0 }"
 
 else 
 	nvidia-settings --assign CurrentMetaMode="DPY-2: 1440x900 @1440x900"
