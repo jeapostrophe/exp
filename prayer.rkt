04158f66a71df76fd3ff1b062b01213b698be036
Jay McCarthy <jay.mccarthy@gmail.com>
2012-11-15 11:00:01 -0700

    Automatic commit at Thu Nov 15 11:00:01 MST 2012

--- prayer.rkt
+++ prayer.rkt
@@ -9,8 +9,53 @@
 (define choices
   (list "Daddy" "Mommy" "Frog" "Peach"))
 
+(define (any-same? l r)
+  (ormap equal? l r))
+(module+ tests
+  (require rackunit)
+  (check-equal? (any-same? (list "D" "M" "F" "P")
+                           (list "D" "P" "F" "M"))
+                #t))
+
+(define (snoc l x)
+  (append l (list x)))
+
+(define (insert s l)
+  (let/ec esc
+    (let loop ([l l])
+      (match l
+        [(list)
+         (list s)]
+        [(list lst)
+         (if (any-same? s lst)
+           (esc #f)
+           (list s lst))]
+        [(list-rest fst rst)
+         (if (any-same? s fst)
+           (cons fst (loop rst))
+           (cons s l))]))))
+
+(define (optimize il)
+  (let loop ([init-result empty]
+             [to-insert il])
+    (define-values
+      (result failed)
+      (for/fold ([result init-result]
+                 [failed empty])
+          ([s (in-list to-insert)])
+        (define next-result
+          (insert s result))
+        (cond
+          [next-result
+           (values next-result failed)]
+          [else
+           (values result (cons s failed))])))
+    (if (empty? failed)
+      result
+      (loop result failed))))
+
 (define schedules
-  (shuffle
+  (optimize
    (for*/list ([b (in-list choices)]
                [l (in-list (remove* (list b "Daddy") choices))]
                [d (in-list (remove* (list b l) choices))]
@@ -39,24 +84,33 @@
             "Friday"
             "Saturday"))
 
+  (define (next-times)
+    (for/list ([i (in-range how-many-days)])
+      (seconds->date (+ (current-seconds) (* 24 60 60 i)))))
+
+  (define (next-schedules)
+    (for/list ([t (in-list (next-times))])
+      (schedule-of-time t)))
+
+  (for ([i (in-range 4)])
+    (for ([s (in-list (next-schedules))])
+      (printf "~a\t" (list-ref s i)))
+    (newline))
+  (exit 1)
+
   (define (start req)
-    (define ts
-      (for/list ([i (in-range how-many-days)])
-        (seconds->date (+ (current-seconds) (* 24 60 60 i)))))
+    (define ts (next-times))
     (define these-schedules
-      (for/list ([t (in-list ts)])
-        (schedule-of-time t)))
+      (next-schedules))
     (response/xexpr
      `(html
        (head
         (title "Prayer Chart")
-        (style #<<END
-          table tr td:nth-child(1) {
+        (style
+            "table tr td:nth-child(1) {
                                     font-weight: bold;
                                                  text-align: right;
-                                                 }
-END
-          ))
+                                                 }"))
        (body
         (table
          (thead
