9ad3cf5be6be6deb55d47abff3527cb2ae5447e5
Jay McCarthy <jay.mccarthy@gmail.com>
2013-02-13 06:00:01 -0700

    Automatic commit at Wed Feb 13 06:00:01 MST 2013

--- bin/bm.sh
+++ bin/bm.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-while true ; do
+while sleep 1 ; do
     pkill batterymon
     pkill -9 batterymon
+    echo restarting
     batterymon -t 16x16
+    if [ $? = 139 ] ; then
+	exit 1
+    fi
 done
