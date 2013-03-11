93eb2faec35203ed632b274749bcfe379a8a4ab6
Jay McCarthy <jay.mccarthy@gmail.com>
2013-03-11 12:22:51 -0600

    up

--- coq/NonDet.v
+++ coq/NonDet.v
@@ -41,6 +41,17 @@ Inductive aeval : state -> aexp -> nat -> Prop :=
     aeval s (APlus le re) (lv + rv).
 Hint Constructors aeval.
 
+Theorem aeval_det:
+ forall s ae n_0 n_1,
+   aeval s ae n_0 ->
+   aeval s ae n_1 ->
+   n_0 = n_1.
+Proof.
+  intros s ae n_0 n_1 A1. generalize dependent n_1.
+  induction A1; intros n_1 A2; inversion A2; subst; auto.
+Qed.
+Hint Resolve aeval_det.
+
 Inductive bexp : Type :=
 | BEq : aexp -> aexp -> bexp.
 Hint Constructors bexp.
@@ -52,6 +63,17 @@ Inductive beval : state -> bexp -> bool -> Prop :=
     aeval s re rv ->
     beval s (BEq le re) (beq_nat lv rv).
 Hint Constructors beval.
+
+Theorem beval_det:
+ forall s be b_0 b_1,
+   beval s be b_0 ->
+   beval s be b_1 ->
+   b_0 = b_1.
+Proof.
+  intros s ae n_0 n_1 A1. generalize dependent n_1.
+  induction A1; intros n_1 A2; inversion A2; subst; eauto.
+Qed.
+Hint Resolve beval_det.
 
 Inductive com : Type :=
 | CAss : id -> aexp -> com
@@ -75,6 +97,23 @@ Inductive ceval : state -> com -> state -> Prop :=
     ceval s (CIf be ct cf) s'.
 Hint Constructors ceval.
 
+Theorem ceval_det:
+  forall s c s_0 s_1,
+    ceval s c s_0 ->
+    ceval s c s_1 ->
+    s_0 = s_1.
+Proof.
+  intros s c s_0 s_1 C1. generalize dependent s_1.
+  induction C1; intros s_1 C2; inversion C2; subst; eauto.
+  absurd (true = false).
+   intros TF. inversion TF.
+   eapply beval_det. apply H. apply H5.
+  absurd (false = true).
+   intros TF. inversion TF.
+   eapply beval_det. apply H. apply H5.
+Qed.
+Hint Resolve ceval_det.
+
 Definition thread := list com.
 Hint Unfold thread.
 
@@ -84,6 +123,20 @@ Inductive teval : state -> thread -> state -> thread -> Prop :=
     ceval s c_0 s' ->
     teval s (c_0::ts) s' ts.
 Hint Constructors teval.
+
+Theorem teval_det:
+  forall s t s_0 t_0 s_1 t_1,
+    teval s t s_0 t_0 ->
+    teval s t s_1 t_1 ->
+    (s_0 = s_1) /\ (t_0 = t_1).
+Proof.
+  intros s t s_0 t_0 s_1 t_1 T1.
+   generalize dependent s_1. 
+   generalize dependent t_1.
+  induction T1 as [s c_0 ts s' C].
+  intros t_1 s_1 T2. inversion T2; subst; eauto.
+Qed.
+Hint Resolve teval_det.
 
 Inductive tseval_nondet : state -> list thread -> state -> Prop :=
 | Done_nondet :
@@ -178,6 +231,28 @@ Inductive tseval_det : schedule -> state -> list thread -> state -> Prop :=
     tseval_det (sch_0 :: sch_n) s  ts  s''.
 Hint Constructors tseval_det.
 
+Lemma app_length:
+  forall X (l1:list X) l3 l2 l4,
+    length l1 = length l3 ->
+    l1 ++ l2 = l3 ++ l4 ->
+    l1 = l3.
+Proof.
+  intros X. induction l1 as [|x l1]; induction l3 as [|y l3]; simpl;
+  intros l2 l4 EQ; inversion EQ; auto.
+  intros EQ2. inversion EQ2; subst.
+  erewrite IHl1. reflexivity.
+  auto. exact H2.
+Qed.
+
+Lemma app_tail:
+  forall X (l1:list X) x1 x2 l2 l3,
+    l1 ++ x1 :: l2 = l1 ++ x2 :: l3 ->
+    x1 = x2.
+Proof.
+  intros X. induction l1 as [|x0 l1]; 
+   simpl; intros x1 x2 l2 l3 EQ; inversion EQ; subst; eauto.
+Qed.
+
 Theorem tseval_det_is:
   forall sch s ts s' s'',
     tseval_det sch s ts s' ->
@@ -199,12 +274,25 @@ Proof.
    inversion H0.
 
    eapply IHE1. 
-   replace t0 with t in *.
-   replace ts_before0 with ts_before in *.
-   replace ts_after0 with ts_after in *.
-   replace s'0
+   replace ts_before0 with ts_before in *; eauto.
+   replace t0 with t in *; eauto.
+   replace ts_after0 with ts_after in *; eauto.
+   replace s'0 with s' in *; eauto.
+   replace t'0 with t' in *; eauto.
+   eapply teval_det; eauto.
+   eapply teval_det; eauto.
+    apply app_inv_head in H2.
+    inversion H2. auto.
 
-Admitted.
+   eapply app_tail.
+    exact H2.
+
+   symmetry.
+    erewrite app_length.
+    reflexivity. 
+    symmetry. apply H3.
+    exact H2.
+Qed. 
 
 Theorem tseval_non_to_det:
   forall s ts s',
@@ -213,7 +301,13 @@ Theorem tseval_non_to_det:
       tseval_det sch s ts s'.
 Proof.
   intros s ts s' TND.
-  induction TND as [s c_0 s' ts ts_before c_n ts_after s'' C EQts N].
+  induction TND 
+   as [s ts NoRun|s c_0 s' ts ts_before c_n ts_after s'' C EQts N].
+
+  exists nil.
+  eapply Done_det.
+  apply NoRun.
+
   destruct IHN as [sch_n IHN].
   eexists.
   eapply Thread_det.
@@ -229,7 +323,11 @@ Theorem tseval_det_to_non:
     tseval_nondet s ts s'.
 Proof.
   intros sch s ts s' TD.
-  induction TD as [s c_0 s' ts sch_0 ts_before c_n ts_after  sch_n s'' C EQts LENtsb N].
+  induction TD as [s ts ts' NoRun|s c_0 s' ts sch_0 ts_before c_n ts_after  sch_n s'' C EQts LENtsb N].
+
+  eapply Done_nondet.
+  apply NoRun.
+
   eapply Thread_nondet.
   apply C.
   apply EQts.
