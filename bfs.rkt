c13633fc041029fbb6579613be2f775458a3a9c9
Jay McCarthy <jay.mccarthy@gmail.com>
2013-03-21 08:30:58 -0600

    up

--- bfs.rkt
+++ bfs.rkt
@@ -1,22 +1,45 @@
-#lang racket
-(require data/queue)
+#lang racket/base
+(require racket/list
+         data/heap)
 
-(define (enqueue!* q l)
-  (for-each (curry enqueue! q) l))
+(define (heap-empty? h)
+  (zero? (heap-count h)))
 
-(define (bfs ? node-data node-children root)
-  (define q (make-queue))
-  (let visit ([n root])
+(define (best-first-search node-data node-children node-score ? root)
+  (define (node-<= x y)
+    (<= (node-score x) (node-score y)))
+  (define q (make-heap node-<=))
+  (let visit ([i 0] [n root])
     (cond
       [(? (node-data n))
+       (eprintf "found after ~a\n" i)
        n]
       [else
-       (enqueue!* q (node-children n))
+       (heap-add-all! q (node-children n))
        (cond
-         [(queue-empty? q)
+         [(heap-empty? q)
           #f]
          [else
-          (visit (dequeue! q))])])))
+          (define next (heap-min q))
+          (heap-remove-min! q)
+          (visit (add1 i) next)])])))
+
+(define (bfs node-data node-children ? root)
+  (best-first-search
+   node-data node-children
+   (λ (n) 0)
+   ?
+   root))
+
+(define (dfs node-data node-children ? root)
+  (cdr
+   (best-first-search
+    (λ (d*n) (node-data (cdr d*n)))
+    (λ (d*n) (map (λ (c) (cons (add1 (car d*n)) c))
+                  (node-children (cdr d*n))))
+    (λ (d*n) (* -1 (car d*n)))
+    ?
+    (cons 0 root))))
 
 (module+ main
   (define d
@@ -42,18 +65,47 @@
        (20))
       (21)))
 
-  (bfs (λ (x)
-         (printf "Visited ~a\n" x)
-         (= x 11))
-       first
+  (define look-for-11
+    (λ (x)
+      (printf "Visited ~a\n" x)
+      (= x 11)))
+
+  (bfs first
        rest
+       look-for-11
        d)
 
-  (bfs (λ (x)
-         (printf "Visited ~a\n" x)
-         (= x 11))
-       (λ (x) x)
+  (bfs (λ (x) x)
+       (λ (x) (list (+ (* 2 x) 0)
+                    (+ (* 2 x) 1)))
+       look-for-11
+       1)
+
+  (dfs first
+       rest
+       look-for-11
+       d)
+
+  (dfs (λ (x) x)
        (λ (x)
-         (list (+ (* 2 x) 0)
-               (+ (* 2 x) 1)))
-       1))
+         (if (> x 150)
+           empty
+           (list (+ (* 2 x) 0)
+                 (+ (* 2 x) 1))))
+       look-for-11
+       1)
+
+  (best-first-search
+   first
+   rest
+   (λ (x) (abs (- (first x) 11)))
+   look-for-11
+   d)
+
+  (best-first-search
+   (λ (x) x)
+   (λ (x) (list (+ (* 2 x) 0)
+                (+ (* 2 x) 1)))
+   (λ (x) (abs (- x 11)))
+   look-for-11
+   1))
