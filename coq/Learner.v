ad463d838c7756323c635102d5d3ccb4df738316
Jay McCarthy <jay.mccarthy@gmail.com>
2013-03-22 15:00:02 -0600

    Automatic commit at Fri Mar 22 15:00:02 MDT 2013

--- coq/Learner.v
+++ coq/Learner.v
@@ -30,10 +30,18 @@ Module Learning ( Export Domain : LearningDomain ).
   Definition Oracle := forall (q:query), answer q.
   Hint Unfold Oracle.
 
-  Definition Oracle_correct (f:function) (o:Oracle) :=
-    (forall x y, o (membership x) = membership_answer x y -> f x = y)
-    /\ (forall f' x y, o (correct f') = correct_answer_counter f' x y -> (function_spec_eval f') x <> y /\ f x = y)
-    /\ (forall f', o (correct f') = correct_answer_good f' -> forall x, (function_spec_eval f') x = f x).
+  Definition Oracle_correct (f:function_spec) (o:Oracle) :=
+    (forall x y,
+      o (membership x) = membership_answer x y -> 
+      (function_spec_eval f) x = y)
+    /\ (forall f' x y, 
+      o (correct f') = correct_answer_counter f' x y -> 
+      (function_spec_eval f') x <> y
+      /\ (function_spec_eval f) x = y)
+    /\ (forall f', 
+      o (correct f') = correct_answer_good f'
+      -> forall x,
+        (function_spec_eval f') x = (function_spec_eval f) x).
   Hint Unfold Oracle_correct.
 
   Definition Learner := Oracle -> function_spec.
@@ -43,7 +51,7 @@ Module Learning ( Export Domain : LearningDomain ).
     forall f,
       Oracle_correct f o ->
       forall x,
-        (function_spec_eval (l o)) x = f x.
+        (function_spec_eval (l o)) x = (function_spec_eval f) x.
   Hint Unfold Learner_correct.
 End Learning.
 
@@ -105,8 +113,8 @@ match q with
 end.
 
 Theorem lagrange_oracle_correct :
-  forall cs,
-    Oracle_correct (function_spec_eval cs) (lagrange_oracle cs).
+  forall ps,
+    Oracle_correct ps (lagrange_oracle ps).
 Proof.
   intros cs. unfold Oracle_correct, lagrange_oracle.
   split; [|split]; simpl.
@@ -125,7 +133,13 @@ Proof.
   intros H. inversion H.
 Qed.
 
-Definition lagrange_learner (o:Oracle) (ps:list (nat*nat)) (n:nat) :=
+(* The idea is that we are constructing a Lagrangian Polynomial for
+the data and whenever we're wrong, there's another data point we
+should have added. The number represents the degree of the polynomial,
+or the number of data points. The ps should have the invariant that
+they are all valid readings of the function. *)
+
+Fixpoint lagrange_learner (ps:list (nat*nat)) (n:nat) (o:Oracle) :=
 match n with 
 | 0 =>
   ps
@@ -134,49 +148,80 @@ match n with
     | correct_answer_good _ =>
       ps
     | correct_answer_counter _ x y =>
-      lagrange_learner o ((x,y)::ps) n
+      lagrange_learner ((x,y)::ps) n o
   end
 end.
 
+Theorem lagrange_equality :
+  forall ps l,
+    (length ps) = (length l) ->
+    (forall x y, In (x,y) l -> function_spec_eval ps x = y) ->
+    forall x,
+      function_spec_eval ps x = function_spec_eval l x.
+Proof.
+ (* The intuition is that 
+Admitted.
 
-  Axiom function_spec_dec :
-    forall (f f':function), {x | f x <> f' x} + {f = f'}.
+Theorem lagrange_learner_correct_general :
+  forall n ps l,
+      (length ps) <= (length l) + n ->
+      (forall x y, In (x,y) l -> function_spec_eval ps x = y) ->
+      Learner_correct (lagrange_oracle ps) (lagrange_learner l n).
+Proof.
+  induction n as [|n]; simpl.
 
-  Definition pipe_oracle (f:function) : Oracle :=
-    fun q =>
-      match q with
-        | membership x =>
-          membership_answer x (f x)
-        | correct f' =>
-          match function_equality_dec f f' with
-            | inright _ =>
-              correct_answer_good f'
-            | inleft (exist x _) =>
-              correct_answer_counter f' x (f x)
-          end
-      end.
+  intros ps l LEN Pl f. 
+  intros [member_correct [correct_good_correct correct_counter_correct]].
+  remember (function_spec_dec ps l) as DEC.
+  destruct DEC as [EQ|NEQ].
+  eapply correct_counter_correct. simpl.
+  rewrite <- HeqDEC. reflexivity.
 
-  Definition pipe_learner (o:Oracle) :=
-    fun x =>
-      match o (membership x) with
-        | membership_answer _ y =>
-          y
-        | _ =>
-          0
-      end.
+  symmetry. eapply member_correct. simpl.
 
-  Theorem pipe_oracle_learner_correct:
-    forall f,
-      Learner_correct (pipe_oracle f) (pipe_learner).
-  Proof.
-    intros f.
+  induction l as [|[x y] l].
+   destruct ps as [|p ps].
+    symmetry. apply member_correct. reflexivity.
+    simpl in LEN. inversion LEN.
+   simpl.
+   destruct (nat_eq_dec x x').
+    symmetry. apply member_correct. simpl.
+    replace x'. erewrite Pl. reflexivity.
+    simpl. auto.
 
-    unfold Learner_correct, Oracle_correct, pipe_learner, pipe_oracle.
+    eapply IHl.
+    intros xx yy IN.
+    apply Pl. simpl. auto.
 
-    intros f'.
-    intros [membership_correct [correct_counter_correct correct_good_correct]].
-    intros x.
-    symmetry.
-    eapply membership_correct.
-    reflexivity.
-  Qed.
+  intros ps l LEN. unfold Learner_correct.
+  intros Pl.
+  intros f.
+  intros [member_correct [correct_good_correct correct_counter_correct]].
+  intros x.
+  simpl.
+  destruct (function_spec_dec ps l).
+  simpl. symmetry. apply member_correct. simpl.
+  rewrite e. reflexivity.
+  destruct (lagrange_counter_example ps l n0) as [x' NEQ].
+  eapply IHn.
+  
+  admit.
+
+  intros xx yy Inl.
+  eapply Pl. simpl in Inl.
+  destruct Inl as [ EQ | Inl ]; auto.
+  inversion EQ. subst xx yy. clear EQ.
+
+
+Admitted.
+
+Theorem lagrange_learner_correct :
+  forall ps,
+    Learner_correct
+     (lagrange_oracle ps)
+     (lagrange_learner nil (length ps)).
+Proof.
+  intros ps. eapply lagrange_learner_correct_general.
+  auto.
+  intros x y IN. inversion IN.
+Qed.
