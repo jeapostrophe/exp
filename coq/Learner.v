2dd0df2d884e563b40f36219c2ad071d9bb2550a
Jay McCarthy <jay.mccarthy@gmail.com>
2013-03-22 16:00:01 -0600

    Automatic commit at Fri Mar 22 16:00:01 MDT 2013

--- coq/Learner.v
+++ coq/Learner.v
@@ -57,171 +57,81 @@ End Learning.
 
 Require Import List.
 
-Module LagrangeLD <: LearningDomain.
-  Definition function_spec := list (nat*nat).
+Module ConstantLD <: LearningDomain.
+  Definition function_spec := nat.
 
   Theorem function_spec_dec :
     forall (x y:function_spec),
       { x = y } + { x <> y }.
   Proof.
-    decide equality. decide equality.
-    decide equality. decide equality.
+    decide equality. 
   Qed.
 
-  Theorem nat_eq_dec:
-    forall (x y:nat),
-      { x = y } + { x <> y }.
-  Proof.
-    decide equality.
-  Qed.
+  Definition function_spec_eval (p:nat) (x:nat) := p.  
+End ConstantLD.
 
-  Fixpoint function_spec_eval (ps:list (nat*nat)) x' :=
-    match ps with
-      | nil =>
-        0
-      | (x,y) :: ps =>
-        if nat_eq_dec x x' then
-          y
-          else
-            function_spec_eval ps x'
-    end.  
-End LagrangeLD.
+Module Import ConstantLearning := Learning ConstantLD.
 
-Module Import LagrangeLearning := Learning LagrangeLD.
-
-Theorem lagrange_counter_example :
-  forall ps ps',
-    ps <> ps' ->
-    { x | function_spec_eval ps x <> function_spec_eval ps' x }.
-Proof.
-Admitted.
-
-Definition lagrange_oracle (ps:list (nat*nat)) (q:query) : answer q :=
+Definition constant_oracle (k:nat) (q:query) : answer q :=
 match q with
 | membership x =>
-  membership_answer x ((function_spec_eval ps) x) 
-| correct ps' =>
-  match function_spec_dec ps ps' with
+  membership_answer x k 
+| correct k' =>
+  match function_spec_dec k k' with
     | left _ =>
-      correct_answer_good ps'
+      correct_answer_good k'
     | right NEQ  =>
-      match lagrange_counter_example ps ps' NEQ with
-        | exist x _ =>
-          correct_answer_counter ps' x ((function_spec_eval ps) x)
-      end
+      correct_answer_counter k' 0 k
   end
 end.
 
-Theorem lagrange_oracle_correct :
-  forall ps,
-    Oracle_correct ps (lagrange_oracle ps).
+Theorem constant_oracle_correct :
+  forall k,
+    Oracle_correct k (constant_oracle k).
 Proof.
-  intros cs. unfold Oracle_correct, lagrange_oracle.
+  intros k. unfold Oracle_correct, constant_oracle.
   split; [|split]; simpl.
 
-  intros x y EQ. inversion_clear EQ. reflexivity.
+  intros x y EQ. inversion_clear EQ.
+  unfold function_spec_eval. reflexivity.
 
-  intros cs' x y.
-  destruct (function_spec_dec cs cs') as [EQ | NEQ].
+  intros k' x y.
+  destruct (function_spec_dec k k') as [EQ | NEQ].
   intros H. inversion H.
-  destruct (lagrange_counter_example cs cs' NEQ) as [z CEX].
   intros H. inversion H. subst x y. auto.
 
-  intros cs'. destruct (function_spec_dec cs cs') as [EQ | NEQ].
-  subst cs'. auto.
-  destruct (lagrange_counter_example cs cs' NEQ) as [z CEX].
+  intros k'. destruct (function_spec_dec k k') as [EQ | NEQ].
+  subst k'. auto.
   intros H. inversion H.
 Qed.
 
-(* The idea is that we are constructing a Lagrangian Polynomial for
-the data and whenever we're wrong, there's another data point we
-should have added. The number represents the degree of the polynomial,
-or the number of data points. The ps should have the invariant that
-they are all valid readings of the function. *)
-
-Fixpoint lagrange_learner (ps:list (nat*nat)) (n:nat) (o:Oracle) :=
-match n with 
-| 0 =>
-  ps
-| S n =>
-  match o (correct ps) with
+Definition constant_learner (o:Oracle) :=
+  match o (correct 0) with
+    | membership_answer _ _ =>
+      (* XXX This case is impossible. It's strange that the type system doesn't rule it out *)
+      0
     | correct_answer_good _ =>
-      ps
+      0
     | correct_answer_counter _ x y =>
-      lagrange_learner ((x,y)::ps) n o
-  end
-end.
+      y
+  end.
 
-Theorem lagrange_equality :
-  forall ps l,
-    (length ps) = (length l) ->
-    (forall x y, In (x,y) l -> function_spec_eval ps x = y) ->
-    forall x,
-      function_spec_eval ps x = function_spec_eval l x.
-Proof.
- (* The intuition is that 
-Admitted.
-
-Theorem lagrange_learner_correct_general :
-  forall n ps l,
-      (length ps) <= (length l) + n ->
-      (forall x y, In (x,y) l -> function_spec_eval ps x = y) ->
-      Learner_correct (lagrange_oracle ps) (lagrange_learner l n).
-Proof.
-  induction n as [|n]; simpl.
-
-  intros ps l LEN Pl f. 
-  intros [member_correct [correct_good_correct correct_counter_correct]].
-  remember (function_spec_dec ps l) as DEC.
-  destruct DEC as [EQ|NEQ].
-  eapply correct_counter_correct. simpl.
-  rewrite <- HeqDEC. reflexivity.
-
-  symmetry. eapply member_correct. simpl.
-
-  induction l as [|[x y] l].
-   destruct ps as [|p ps].
-    symmetry. apply member_correct. reflexivity.
-    simpl in LEN. inversion LEN.
-   simpl.
-   destruct (nat_eq_dec x x').
-    symmetry. apply member_correct. simpl.
-    replace x'. erewrite Pl. reflexivity.
-    simpl. auto.
-
-    eapply IHl.
-    intros xx yy IN.
-    apply Pl. simpl. auto.
-
-  intros ps l LEN. unfold Learner_correct.
-  intros Pl.
-  intros f.
-  intros [member_correct [correct_good_correct correct_counter_correct]].
-  intros x.
-  simpl.
-  destruct (function_spec_dec ps l).
-  simpl. symmetry. apply member_correct. simpl.
-  rewrite e. reflexivity.
-  destruct (lagrange_counter_example ps l n0) as [x' NEQ].
-  eapply IHn.
-  
-  admit.
-
-  intros xx yy Inl.
-  eapply Pl. simpl in Inl.
-  destruct Inl as [ EQ | Inl ]; auto.
-  inversion EQ. subst xx yy. clear EQ.
-
-
-Admitted.
-
-Theorem lagrange_learner_correct :
-  forall ps,
+Theorem constant_learner_correct :
+  forall k,
     Learner_correct
-     (lagrange_oracle ps)
-     (lagrange_learner nil (length ps)).
+     (constant_oracle k)
+     (constant_learner).
 Proof.
-  intros ps. eapply lagrange_learner_correct_general.
-  auto.
-  intros x y IN. inversion IN.
+  intros k.
+  unfold Learner_correct, Oracle_correct, constant_oracle, constant_learner, function_spec_eval.
+  intros f [m_c [cac_c cag_c]].
+  intros x. 
+  remember (function_spec_dec k 0) as EQ.
+  destruct EQ.
+  symmetry. eapply m_c. rewrite e. reflexivity.
+  symmetry. eapply (cac_c 0). rewrite <- HeqEQ.
+  reflexivity.
+
+  Grab Existential Variables.
+  apply 0.
 Qed.
