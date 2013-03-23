ced6ba13b9193e7a1cb80233658bef3cfb4b74fd
Jay McCarthy <jay.mccarthy@gmail.com>
2013-03-22 19:00:01 -0600

    Automatic commit at Fri Mar 22 19:00:01 MDT 2013

--- coq/Learner.v
+++ coq/Learner.v
@@ -1,145 +1,173 @@
 Require Import Omega.
 
-Definition function := nat -> nat.
-Hint Unfold function.
-
 Module Type LearningDomain.
   Parameter function_spec : Set.
   Parameter function_spec_dec :
     forall (x y:function_spec),
       { x = y } + { x <> y }.
+  Parameter Dom : Set.
+  Parameter Rng : Set.
+  Definition function := Dom -> Rng.
   Parameter function_spec_eval :
     function_spec -> function.
 End LearningDomain.
 
 Module Learning ( Export Domain : LearningDomain ).
-  Inductive query : Set :=
-  | membership : nat -> query
-  | correct    : function_spec -> query.
-  Hint Constructors query.
+  Definition Oracle_membership := Dom -> Rng.
+  Definition Oracle_membership_good 
+    (f:function_spec) (om:Oracle_membership) :=
+    forall x y,
+      om x = y -> function_spec_eval f x = y.
 
-  Inductive answer : query -> Set :=
-  | membership_answer : forall (x y:nat),
-    answer (membership x)
-  | correct_answer_counter : forall f (x y:nat),
-    answer (correct f)
-  | correct_answer_good : forall f,
-    answer (correct f).
-  Hint Constructors answer.
+  Definition Oracle_correct := function_spec -> unit + (Dom*Rng).
+  Definition Oracle_correct_right_good
+    (f:function_spec) (oc:Oracle_correct) :=
+    forall f' x y,
+      oc f' = inr (x,y) ->
+      function_spec_eval f' x <> y
+      /\ y = function_spec_eval f x.
+  Definition Oracle_correct_left_good 
+    (f:function_spec) (oc:Oracle_correct) :=
+    forall f',
+      oc f' = inl tt ->
+      forall x,
+        function_spec_eval f' x = function_spec_eval f x.
+  Definition Oracle_correct_good
+    (f:function_spec) (oc:Oracle_correct) :=
+    (Oracle_correct_left_good f oc) /\
+    (Oracle_correct_right_good f oc).
 
-  Definition Oracle := forall (q:query), answer q.
-  Hint Unfold Oracle.
-
-  Definition Oracle_correct (f:function_spec) (o:Oracle) :=
-    (forall x y,
-      o (membership x) = membership_answer x y ->
-      (function_spec_eval f) x = y)
-    /\ (forall f' x y,
-      o (correct f') = correct_answer_counter f' x y ->
-      (function_spec_eval f') x <> y
-      /\ (function_spec_eval f) x = y)
-    /\ (forall f',
-      o (correct f') = correct_answer_good f'
-      -> forall x,
-        (function_spec_eval f') x = (function_spec_eval f) x).
-  Hint Unfold Oracle_correct.
+  Definition Oracle := (Oracle_membership * Oracle_correct)%type.
+  Definition Oracle_good
+    (f:function_spec) (o:Oracle) :=
+    (Oracle_membership_good f (fst o))
+    /\ (Oracle_correct_good f (snd o)).
 
   Definition Learner := Oracle -> function_spec.
   Hint Unfold Learner.
 
-  Definition Learner_correct (learner:Learner) :=
+  Definition Learner_good (P:function_spec -> Prop) (learner:Learner) :=
     forall o f,
-      Oracle_correct f o ->
+      P f ->
+      Oracle_good f o ->
       forall x,
         (function_spec_eval (learner o)) x = (function_spec_eval f) x.
-  Hint Unfold Learner_correct.
+  Hint Unfold Learner_good.
 End Learning.
 
 Module Type Learner ( Export Domain : LearningDomain ).
   Module Export L := Learning Domain.
 
   Parameter learner : Learner.
-
-  Parameter learner_correct : Learner_correct learner.
+  Parameter Learner_P : function_spec -> Prop.
+  Parameter learner_good : Learner_good Learner_P learner.
 End Learner.
 
 Module ConstantLD <: LearningDomain.
-  Definition function_spec := nat.
+  Parameter Dom : Set.
+  Parameter Dom_0 : Dom.
+
+  Parameter Rng : Set.
+  Parameter Rng_0 : Rng.
+
+  Parameter Rng_dec :
+    forall (x y:Rng),
+      { x = y } + { x <> y }.
+
+  Definition function := Dom -> Rng.
+
+  Definition function_spec := Rng.
 
   Theorem function_spec_dec :
     forall (x y:function_spec),
       { x = y } + { x <> y }.
   Proof.
-    decide equality.
+    apply Rng_dec.
   Qed.
 
-  Definition function_spec_eval (p:nat) (x:nat) := p.
+  Definition function_spec_eval (p:Rng) (x:Dom) : Rng := p.
 End ConstantLD.
 
 Module ConstantLearner <: Learner ConstantLD.
   Module Export L := Learning ConstantLD.
 
   Definition learner (o:Oracle) :=
-    match o (correct 0) with
-      | membership_answer _ _ =>
-      (* XXX This case is impossible. It's strange that the type system doesn't rule it out *)
-        0
-      | correct_answer_good _ =>
-        0
-      | correct_answer_counter _ x y =>
+    match (snd o) Rng_0 with
+      | inl _ =>
+        Rng_0
+      | inr (x,y) =>
         y
     end.
 
-  Theorem learner_correct : Learner_correct learner.
+  Definition Learner_P (f:function_spec) := True.
+
+  Theorem learner_good : Learner_good Learner_P learner.
   Proof.
-    intros o k.
-    unfold Learner_correct, Oracle_correct, learner, function_spec_eval.
-    intros [m_c [cac_c cag_c]].
-    intros x.
-    remember k (correct 0).
-    remember (function_spec_dec k 0) as EQ.
-    destruct EQ.
-    symmetry. eapply m_c. rewrite e. reflexivity.
-    symmetry. eapply (cac_c 0). rewrite <- HeqEQ.
+    intros [om oc] f Pf [om_g [oc_lg oc_rg]] x.
+    unfold learner. simpl in *.
+    remember (oc Rng_0) as oc0.
+    destruct oc0.
+   
+    apply oc_lg. rewrite <- Heqoc0. destruct u; auto.
+
+    destruct p as [xx yy].
+    symmetry in Heqoc0.
+    eapply oc_rg in Heqoc0.
+    destruct Heqoc0 as [NEQ EQ].
+    subst yy. unfold function_spec_eval.
     reflexivity.
-
-    Grab Existential Variables.
-    apply 0.
   Qed.
-
 End ConstantLearner.
 
-Module Import ConstantLearning := Learning ConstantLD.
+(* XXX Can't figure out how to instantiate ConstantLD with Dom = Rng =
+nat *)
 
-Definition constant_oracle (k:nat) (q:query) : answer q :=
-  match q with
-    | membership x =>
-      membership_answer x k
-    | correct k' =>
-      match function_spec_dec k k' with
-        | left _ =>
-          correct_answer_good k'
-        | right NEQ  =>
-          correct_answer_counter k' 0 k
-      end
-  end.
+Axiom lagrange : list (nat*nat) -> nat -> nat.
+Definition lagrange_correct f n :=
+  forall l, 
+    length l <= n -> 
 
-Theorem constant_oracle_correct :
-  forall k,
-    Oracle_correct k (constant_oracle k).
-Proof.
-  intros k. unfold Oracle_correct, constant_oracle.
-  split; [|split]; simpl.
+Module PolyLD <: LearningDomain.
+  Definition function_spec := list (nat*nat).
+  Theorem function_spec_dec:
+    forall (x y:function_spec),
+      { x = y } + { x <> y }.
+  Proof.
+    decide equality. decide equality.
+    decide equality. decide equality.
+  Qed.
+  Definition Dom := nat.
+  Definition Rng := nat.
+  Definition function := Dom -> Rng.
+  Definition function_spec_eval (fs:function_spec) := lagrange fs.
+End PolyLD.
 
-  intros x y EQ. inversion_clear EQ.
-  unfold function_spec_eval. reflexivity.
+Module PolyLearner <: Learner PolyLD.
+  Module Export L := Learning PolyLD.
 
-  intros k' x y.
-  destruct (function_spec_dec k k') as [EQ | NEQ].
-  intros H. inversion H.
-  intros H. inversion H. subst x y. auto.
+  Fixpoint learner (ps:list (nat*nat)) (n:nat) (o:Oracle) :=
+    match n with
+      | 0 =>
+        ps
+      | S n =>
+        match (snd o) ps with
+          | inl _ =>
+            ps
+          | inr (x, y) =>
+            learner ((x,y)::ps) n o
+        end
+    end.
 
-  intros k'. destruct (function_spec_dec k k') as [EQ | NEQ].
-  subst k'. auto.
-  intros H. inversion H.
-Qed.
+  Definition Learner_P := lagrange_correct f n.
+
+  Theorem learner_good : 
+    forall o f,
+      Oracle_good f o ->
+      forall x,
+        (function_spec_eval (learner o)) x = (function_spec_eval f) x.
+
+    forall n,
+      Learner_good (fun f => ) (learner nil n)
+
+      (fun o =>  o f).
+  Proof.
