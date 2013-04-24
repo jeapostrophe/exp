ccc6ab54422b7bd3c5c40c55e555fcae7541e821
Jay McCarthy <jay.mccarthy@gmail.com>
2013-04-24 14:00:02 -0600

    Automatic commit at Wed Apr 24 14:00:02 MDT 2013

--- anki-monster/main.py
+++ anki-monster/main.py
@@ -1,17 +1,38 @@
+from anki.hooks import addHook
 from aqt import mw
 from aqt.utils import showInfo
 from aqt.qt import *
+
+N = 50
 
 def monster():
     newCount, lrnCount, revCount = mw.col.sched.counts()
     totalCount = (newCount + lrnCount + revCount)
     howManyToDo = totalCount / 2
-    if howManyToDo < 50:
-        howManyToDo = 50
-    if totalCount < 50:
+    if howManyToDo < N:
+        howManyToDo = N
+    if totalCount < N:
         howManyToDo = totalCount
 
-    mw.reviewer.nextCard()
+    mw.monsterToDo = howManyToDo
+
+    if mw.monsterToDo > 0:
+        mw.moveToState("review")
+
+def monsterAnswerCard(card, ease):
+    if ease == 1:
+        return
+
+    mw.monsterToDo = mw.monsterToDo - 1
+
+    if mw.monsterToDo <= 0:
+        mw.moveToState("deckBrowser")
+
+def monsterShowQuestion():
+    showInfo("Current monsters: %d" % mw.monsterToDo)
+
+addHook('answerCard', monsterAnswerCard)
+addHook('showQuestion', monsterShowQuestion)
 
 action = QAction("Monster", mw)
 mw.connect(action, SIGNAL("triggered()"), monster)
