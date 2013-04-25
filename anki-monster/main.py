6ba413c3ebb233a532cb1cf498ed8e7e23392465
Jay McCarthy <jay.mccarthy@gmail.com>
2013-04-24 19:00:02 -0600

    Automatic commit at Wed Apr 24 19:00:02 MDT 2013

--- anki-monster/main.py
+++ anki-monster/main.py
@@ -2,8 +2,15 @@ from anki.hooks import addHook
 from aqt import mw
 from aqt.utils import showInfo
 from aqt.qt import *
+import os, random
 
 N = 50
+MonstersPath = "/home/jay/Downloads/anki-monster-pngs"
+
+def loadMonster():
+    path = random.choice(os.listdir(MonstersPath))
+    pixmap = QPixmap(path)
+    return pixmap
 
 def monster():
     newCount, lrnCount, revCount = mw.col.sched.counts()
@@ -15,23 +22,48 @@ def monster():
         howManyToDo = totalCount
 
     mw.monsterToDo = howManyToDo
+    mw.monstersDone = 0
+    mw.monsters = [ loadMonster() for n in range(howManyToDo)]
 
     if mw.monsterToDo > 0:
+        displayMonsters()
         mw.moveToState("review")
 
-# XXX Either put a display of monster into the Web view here:
-def monsterShowQuestion():
-    showInfo("Current monsters: %d" % mw.monsterToDo)
+def displayMonsters():
+    parent = mw.app.activeWindow() or mw
+    mb = QDialog(parent)
+    mb.setWindowModality(Qt.WindowModal)
 
-addHook('showQuestion', monsterShowQuestion)
+    outer = QVBoxLayout()
+    mb.setLayout(outer)
 
-# XXX Or, make it so if we don't return, then display the set of monsters
+    dead_layout = QHBoxLayout()
+    outer.addWidget(dead_layout)
+
+    dead = QLabel("Dead: %d" % mw.monstersDone)
+    dead_layout.addWidget(dead)
+
+    alive_layout = QHBoxLayout()
+    outer.addWidget(alive_layout)
+
+    alive = QLabel("Alive: %d" % mw.monsterToDo)
+    alive_layout.addWidget(alive)
+
+    b = QPushButton("Shoot")
+    b.setDefault(True)
+    mb.connect(b, SIGNAL('clicked()'), mb.accept)
+    outer.addWidget(b)
+
+    mb.exec_()
 
 def monsterAnswerCard(card, ease):
     if ease == 1:
         return
 
+    mw.monstersDone = mw.monstersDone + 1
     mw.monsterToDo = mw.monsterToDo - 1
+
+    displayMonsters()
 
     if mw.monsterToDo <= 0:
         mw.moveToState("deckBrowser")
