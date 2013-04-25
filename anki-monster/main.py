2edc8d5ad8b8d80b341fab865c547de985c1924c
Jay McCarthy <jay.mccarthy@gmail.com>
2013-04-25 11:44:02 -0600

    up

--- anki-monster/main.py
+++ anki-monster/main.py
@@ -3,13 +3,14 @@ from aqt import mw
 from aqt.utils import showInfo
 from aqt.qt import *
 import os, random
+from random import shuffle
 
 N = 50
 MonstersPath = "/home/jay/Downloads/anki-monster-pngs"
 
-def loadMonster():
-    path = random.choice(os.listdir(MonstersPath))
-    pixmap = QPixmap(path)
+def loadMonster(png):
+    path = os.path.join( MonstersPath, png )
+    pixmap = QPixmap(path, "png")
     return pixmap
 
 def monster():
@@ -21,42 +22,57 @@ def monster():
     if totalCount < N:
         howManyToDo = totalCount
 
-    mw.monsterToDo = howManyToDo
+    mw.monstersToDo = howManyToDo
     mw.monstersDone = 0
-    mw.monsters = [ loadMonster() for n in range(howManyToDo)]
+    # XXX If howManyToDo is > than the number of pngs, this is bad
+    mw.monsters = [ loadMonster(png) for png in os.listdir(MonstersPath)]
+    shuffle(mw.monsters)
 
-    if mw.monsterToDo > 0:
+    if mw.monstersToDo > 0:
         displayMonsters(False)
         mw.moveToState("review")
 
-def setScaledPixmap(l, p):
-    sp = l.scaled
+def setScaledPixmap(l, w, h, p):
+    sp = p.scaled(w, h, Qt.KeepAspectRatio)
+    l.setPixmap(sp)
 
 def displayMonsters(shoot_p):
     parent = mw.app.activeWindow() or mw
+
+    w = parent.size().width()
+    h = parent.size().height()
+
     mb = QDialog(parent)
     mb.setWindowModality(Qt.WindowModal)
 
     grid = QGridLayout()
-    grid.setMargin(0)
-    grid.setVerticalSpacing(0)
-    grid.setHorizontalSpacing(0)
-    grid.setRowStretch(1, 100)
-    grid.setRowMinimumHeight(2, 6)
+
+    grid.setMargin(1)
+    grid.setVerticalSpacing(1)
+    grid.setHorizontalSpacing(1)
+
+    monster_h = h / 3
+    monster_w = w / (mw.monstersToDo + mw.monstersDone + 1)
+    monster_align = (Qt.AlignHCenter) | ( Qt.AlignBottom )
+
+    grid.setRowMinimumHeight(0, monster_h)
+    grid.setRowMinimumHeight(1, monster_h)
+
+    # XXX The drawings are ugly, because of the black background
 
     dead = QLabel("Dead")
     grid.addWidget( dead, 0, 0, 1, 1, Qt.AlignCenter )
     for i in xrange(mw.monstersDone):
         l = QLabel()
-        setScaledPixmap( l, mw.monsters[ i ] )
-        grid.addWidget( l, 0, i + 1, Qt.AlignCenter )
+        setScaledPixmap( l, monster_w, monster_h, mw.monsters[ i ] )
+        grid.addWidget( l, 0, i + 1, monster_align )
 
     alive = QLabel("Alive")
     grid.addWidget( alive, 1, 0, 1, 1, Qt.AlignCenter )
-    for i in xrange(mw.monsterToDo):
+    for i in xrange(mw.monstersToDo):
         l = QLabel()
-        setScaledPixmap( l, mw.monsters[ mw.monstersDone + i ] )
-        grid.addWidget( l, 1, i + 1, Qt.AlignCenter )
+        setScaledPixmap( l, monster_w, monster_h, mw.monsters[ mw.monstersDone + i ] )
+        grid.addWidget( l, 1, i + 1, monster_align )
 
     b = QPushButton("Shoot" if shoot_p else "Next")
     b.setDefault(True)
@@ -70,12 +86,14 @@ def monsterAnswerCard(card, ease):
     if ease == 1:
         return
 
-    mw.monstersDone = mw.monstersDone + 1
-    mw.monsterToDo = mw.monsterToDo - 1
-
     displayMonsters(True)
 
-    if mw.monsterToDo <= 0:
+    mw.monstersDone = mw.monstersDone + 1
+    mw.monstersToDo = mw.monstersToDo - 1
+
+    displayMonsters(False)
+
+    if mw.monstersToDo <= 0:
         mw.moveToState("deckBrowser")
 
 addHook('answerCard', monsterAnswerCard)
