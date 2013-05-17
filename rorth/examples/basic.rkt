e8cb4682995e74c6ea2b56b1609c1c846366c394
Jay McCarthy <jay.mccarthy@gmail.com>
2013-05-17 11:41:24 -0600

    rorth

--- rorth/examples/basic.rkt
+++ rorth/examples/basic.rkt
@@ -5,86 +5,105 @@
 
 (module+ test
   (check-equal?
-   (rorth 2 5 73 -16 + - *)
-   -104)
+   (rorth 2 -52 :*)
+   (list -104))
+  (check-equal?
+   (rorth 5 57 :-)
+   (list -52))
+  (check-equal?
+   (rorth 2 5 57 :- :*)
+   (list -104))
+  (check-equal?
+   (rorth 73 -16 :+)
+   (list 57))
+  (check-equal?
+   (rorth 2 5 73 -16 :+ :- :*)
+   (list -104))
+
   (check-rorth
-   (2 5 73 -16 + - *)
+   (2 5 73 -16)
+   (2 5 73 -16))
+  (check-rorth
+   (2 5 73 -16 :+)
+   (2 5 57))
+  (check-rorth
+   (2 5 73 -16 :+ :-)
+   (2 -52))
+  (check-rorth
+   (2 5 73 -16 :+ :- :*)
    (-104))
 
   (check-rorth
-   (2 5 73 -16 drop)
+   (2 5 73 -16 :drop)
    (2 5 73))
   (check-rorth
-   (2 5 73 -16 swap)
+   (2 5 73 -16 :swap)
    (2 5 -16 73))
   (check-rorth
-   (2 5 73 -16 rot)
+   (2 5 73 -16 :rot)
    (2 73 -16 5))
   (check-rorth
-   (2 5 73 -16 dup)
+   (2 5 73 -16 :dup)
    (2 5 73 -16 -16))
   (check-rorth
-   (2 5 73 -16 over)
+   (2 5 73 -16 :over)
    (2 5 73 -16 73))
   (check-rorth
-   (2 5 73 -16 tuck)
-   (2 5 73 -16 -16))
+   (2 5 73 -16 :tuck)
+   (2 5 -16 73 -16))
   (check-rorth
-   (2 5 73 -16 3 pick)
-   (2 5 73 -16 2))
-  (check-rorth
-   (2 5 73 -16 3 roll)
-   (5 73 -16 2)))
+   (2 5 73 -16 3 :pick)
+   (2 5 73 -16 2)))
 
-(define/rorth sum-of-squares (a b -- x)
-  dup * swap dup * +)
+(define/rorth :sum-of-squares
+  :dup :* :swap :dup :* :+)
 
 (module+ test
   (check-equal?
-   (sum-of-squares 1 2)
+   (:sum-of-squares 1 2)
    5)
   (check-rorth
-   (1 2 sum-of-squares)
+   (1 2 :sum-of-squares)
    (5)))
 
-(define/rorth square (a -- x)
-  dup *)
+(define/rorth :squared
+  :dup :*)
 
-(define/rorth sum-of-squares2 (a b -- x)
-  squared swap squared +)
+(define/rorth :sum-of-squares2
+  :squared :swap :squared :+)
 
 (module+ test
   (check-equal?
-   (sum-of-squares2 1 2)
+   (:sum-of-squares2 1 2)
    5)
   (check-rorth
-   (1 2 sum-of-squares2)
+   (1 2 :sum-of-squares2)
    (5)))
 
 (define (squared2 x)
   (* x x))
 
-(define-rorth squared2 (a -- x)
-  :squared2)
+(define-rorth :squared2 1
+  squared2)
 
-(define/rorth sum-of-squares3 (a b -- x)
-  squared2 swap squared2 +)
+(define/rorth :sum-of-squares3
+  :squared2 :swap :squared2 :+)
 
 (module+ test
   (check-equal?
-   (sum-of-squares3 1 2)
+   (:sum-of-squares3 1 2)
    5)
   (check-rorth
-   (1 2 sum-of-squares3)
+   (1 2 :sum-of-squares3)
    (5)))
 
 (define (weird x y)
   (values x (* x x) y))
 
-(define-rorth weird (a b -- x y z)
-  :weird)
+(define-rorth :weird 2
+  weird)
 
 (module+ test
   (check-rorth
-   (1 2 weird)
+   (1 2 :weird)
    (1 1 2)))
--- rorth/main.rkt
+++ rorth/main.rkt
@@ -1,45 +1,91 @@
 #lang racket/base
 (require rackunit
          racket/list
+         racket/match
+         racket/stxparam
          (for-syntax racket/base
                      syntax/parse))
 
-(begin-for-syntax
-  (define-syntax-class stack-spec
-    #:attributes (how-many-before how-many-after)
-    (pattern (before:id ... (~datum --) after:id ...)
-             #:attr how-many-before (datum->syntax #f (length (syntax->list #'(before ...))))
-             #:attr how-many-after (length (syntax->list #'(after ...))))))
+(define-syntax-parameter stack
+  (λ (stx) (raise-syntax-error 'stack "Illegal outside rorth" stx)))
+
+(struct stack-op (f)
+        #:property prop:procedure
+        (λ (so . args)
+          (apply values (rorth/stack (reverse args) so))))
+
+(define-syntax (define/raw-rorth stx)
+  (syntax-parse stx
+    [(_ name:id . body)
+     #'(define name
+         (stack-op
+          (λ (this-stack)
+            (syntax-parameterize ([stack (make-rename-transformer #'this-stack)])
+              . body))))]))
 
 (define-syntax (define/rorth stx)
   (syntax-parse stx
-    [(_ name:id sp:stack-spec body ...)
-     #'(define (name stack)
-         (apply values (rorth/stack stack body ...)))]))
+    [(_ name:id body ...)
+     #'(define/raw-rorth name
+         (rorth/stack stack body ...))]))
 
 (define-syntax (define-rorth stx)
   (syntax-parse stx
-    [(_ name:id sp:stack-spec new-name:id)
-     #'(define (new-name stack)
-         (define-values (left-over before) (split-at stack 'sp.how-many-before))
-         (call-with-values (λ () (apply name before))
-           (λ (after)
-             (append left-over after))))]))
+    [(_ new-name:id input:nat name:id)
+     #'(define/raw-rorth new-name
+         (define-values (before left-over) (split-at stack 'input))
+         (call-with-values (λ () (apply name (reverse before)))
+           (λ after (append (reverse after) left-over))))]))
 
 (define-syntax-rule (rorth . body)
-  (rorth/stack . body))
+  (rorth/stack empty . body))
+
+(define (maybe-apply-stack-op e stk)
+  (if (stack-op? e)
+    ((stack-op-f e) stk)
+    (list* e stk)))
 
 (define-syntax (rorth/stack stx)
   (syntax-parse stx
-    [(_ stk:expr r:expr ...+)
-     #'(error 'rorth "~e" '(stk r ...))]))
+    [(_ stk:expr)
+     #'stk]
+    [(_ stk:expr e:expr)
+     #'(maybe-apply-stack-op e stk)]
+    [(_ stk:expr f:expr m:expr ...)
+     #'(rorth/stack (rorth/stack stk f) m ...)]))
 
 (define-syntax-rule (check-rorth (r ...) (a ...))
-  (check-equal? (call-with-values (λ () (rorth r ...))
-                  (λ l l))
+  (check-equal? (reverse (rorth r ...))
                 (list a ...)))
+
+(define/raw-rorth :dup
+  (match-define (list* top rest) stack)
+  (list* top top rest))
+(define/raw-rorth :drop
+  (match-define (list* top rest) stack)
+  rest)
+(define/raw-rorth :swap
+  (match-define (list* a b rest) stack)
+  (list* b a rest))
+(define/raw-rorth :rot
+  (match-define (list* a b c rest) stack)
+  (list* c a b rest))
+(define/raw-rorth :over
+  (match-define (list* a b rest) stack)
+  (list* b a b rest))
+(define/raw-rorth :tuck
+  (match-define (list* a b rest) stack)
+  (list* a b a rest))
+(define/raw-rorth :pick
+  (match-define (list* i rest) stack)
+  (list* (list-ref rest i) rest))
+
+(define-rorth :+ 2 +)
+(define-rorth :- 2 -)
+(define-rorth :* 2 *)
 
 (provide define/rorth
          define-rorth
          rorth
-         check-rorth)
+         check-rorth
+         :pick :tuck :over :rot :drop :dup :+ :- :* :swap)
