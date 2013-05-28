05c9ff62482e4624aaec2a7045500f9e6f587276
Jay McCarthy <jay.mccarthy@gmail.com>
2013-05-28 07:00:02 -0600

    Automatic commit at Tue May 28 07:00:02 MDT 2013

--- nsmv/lang.rkt
+++ nsmv/lang.rkt
@@ -1,22 +1,66 @@
 #lang racket/base
-(require (for-syntax racket/base))
-(provide (except-out (all-from-out racket/base)
-                     #%module-begin)
-         (rename-out [module-begin #%module-begin]))
+(require racket/stxparam
+         racket/splicing
+         (for-syntax racket/base
+                     racket/syntax
+                     racket/list))
+
+(begin-for-syntax
+  (define local-state empty)
+  (define local-state-fs empty))
+
+(define-syntax-rule (define-invocation-local-state id init)
+  (begin
+    (begin-for-syntax
+      (set! local-state
+            (cons
+             #'(id init)
+             local-state)))
+    (define-syntax-parameter id
+      (λ (stx) (raise-syntax-error 'id "Local state" stx)))))
+
+(define-syntax-rule
+  (define-invocation-local-state-dependent-function (f . args) . body)
+  (begin
+    (begin-for-syntax
+      (set! local-state-fs
+            (cons
+             #'(f args body)
+             local-state-fs)))
+    (define-syntax-parameter f
+      (λ (stx) (raise-syntax-error 'f "Local state" stx)))))
 
 (define-syntax (module-begin stx)
   (syntax-case stx ()
     [(_ . body)
-     (with-syntax
-         ([add (datum->syntax stx 'add)]
-          [get (datum->syntax stx 'get)])
+     (with-syntax*
+         ([((local-state-id local-state-init) ...)
+           local-state]
+          [((local-f local-f-args local-f-body) ...)
+           local-state-fs]
+          [(local-f-new-id ...)
+           (generate-temporaries #'(local-f ...))])
        (syntax/loc stx
          (#%module-begin
-          (define state null)
-          (define (add v)
-            (set! state (cons v state)))
+          (define local-state-new-id local-state-init)
+          ...
+          (splicing-syntax-parameterize
+           ([local-state-id (make-rename-transformer #'local-state-new-id)]
+            ...)
+           (define (local-f-new-id . local-f-args) . local-f-body)
+           ...)
+          (splicing-syntax-parameterize
+           ([local-f (make-rename-transformer #'local-f-new-id)]
+            ...)
+           (begin . body)))))]))
 
-          (define (get)
-            state)
+(define-invocation-local-state state null)
+(define-invocation-local-state-dependent-function (add v)
+  (set! state (cons v state)))
+(define-invocation-local-state-dependent-function (get)
+  state)
 
-          . body)))]))
+(provide (except-out (all-from-out racket/base)
+                     #%module-begin)
+         (rename-out [module-begin #%module-begin])
+         add get)
