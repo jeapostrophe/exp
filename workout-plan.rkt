585f6449e6861baf37826007e969d82cc712b1d9
Jay McCarthy <jay.mccarthy@gmail.com>
2013-06-05 20:00:01 -0600

    Automatic commit at Wed Jun  5 20:00:01 MDT 2013

--- workout-plan.rkt
+++ workout-plan.rkt
@@ -19,30 +19,21 @@
     (and (not (equal? v (hash-ref last-ctxt k #f)))
          (cons k v))))
 
-(define format-what
-  (match-lambda
-   ['weight "weights"]
-   ['peg "peg"]
-   ['where ""]
-   ['bar "bar"]
-   ['seat "seat"]
-   ['seat-pos "base"]))
-
 (define format-to
   (match-lambda*
-   [(list 'weight 'back) "Rear"]
-   [(list 'weight 'front) "Front"]
-   [(list 'peg to) (symbol->string to)]
-   [(list 'seat to) (symbol->string to)]
-   [(list 'bar 'there) "Gym"]
-   [(list 'bar 'removed) "side"]
-   [(list 'seat-pos 'side) "side"]
-   [(list 'seat-pos 'front) "front"]
-   [(list 'where 'gym:standing) "Standing"]
-   [(list 'where 'gym:seated) "Seat"]
-   [(list 'where 'pull-up-bar:free) "Bar on Floor"]
-   [(list 'where 'pull-up-bar:above) "Bar on Door"]
-   [(list 'where 'free) "Floor"]))
+   [(list 'weight 'back) "Move weights to rear"]
+   [(list 'weight 'front) "Move weights to front"]
+   [(list 'peg to) (format "Move peg to ~a" to)]
+   [(list 'seat to) (format "Move seat to ~a" to)]
+   [(list 'bar 'there) "Return bar"]
+   [(list 'bar 'removed) "Move bar to side"]
+   [(list 'seat-pos 'side) "Move base to side"]
+   [(list 'seat-pos 'front) "Move base to front"]
+   [(list 'where 'gym:standing) "Move to standing"]
+   [(list 'where 'gym:seated) "Move to seated"]
+   [(list 'where 'pull-up-bar:free) "Move to bar on floor"]
+   [(list 'where 'pull-up-bar:above) "Move to bar on door"]
+   [(list 'where 'free) "Move to floor"]))
 
 (define-runtime-path images-dir ".workout-plan")
 
@@ -50,13 +41,20 @@
   (match es
     [(list)
      (values last-ctxt empty)]
+    [(list-rest (? string? m) es)
+     (define-values (final-ctxt rest-plan) (exercise-plan last-ctxt es))
+     (values
+      final-ctxt
+      (cons
+       (format "\\action{~a}" m)
+       rest-plan))]
     [(list-rest (a what to) es)
      (define new-ctxt (hash-set last-ctxt what to))
      (define-values (final-ctxt rest-plan) (exercise-plan new-ctxt es))
      (values
       final-ctxt
       (cons
-       (format "\\action{~a}{~a}" (format-what what) (format-to what to))
+       (format "\\action{~a}" (format-to what to))
        rest-plan))]
     [(list-rest (e sets reps-per-seat ctxt name) es)
      (cond
@@ -140,7 +138,8 @@
         "shrug")
      (a 'weight 'back)
      (e 5  10 '((where gym:standing) (seat-pos side) (weight back) (peg bottom))
-        "tricep press")))
+        "tricep press")
+     "Congraturations!"))
   (draw (hasheq 'seat-pos 'side
                 'seat 'down
                 'where 'free
