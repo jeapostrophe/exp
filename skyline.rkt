cd0c60feb22abc70d8c5a9a1239aec94b011bfc2
Jay McCarthy <jay.mccarthy@gmail.com>
2013-06-25 11:41:00 -0600

    golf

--- skyline.rkt
+++ skyline.rkt
@@ -1,47 +1,42 @@
 #lang racket/base
-(require racket/list
-         data/heap
-         racket/match)
+(require data/heap)
 
-(define (lift <= f)
-  (λ (x y)
-    (<= (f x) (f y))))
+(define (lift < f)
+  (λ (x y) (< (f x) (f y))))
 
-(define-syntax-rule (while cond body ...)
-  (let loop () (when cond body ... (loop))))
+(define-syntax-rule (while cond body)
+  (let loop () (when cond body (loop))))
 
 (define (skyline bs)
-  (define active-bs (make-heap (lift > second)))
-  (heap-add! active-bs (list -inf.0 0 +inf.0))
+  (define active-bs (make-heap (lift > cadr)))
+  (heap-add! active-bs (list #f 0 #f))
   (define ended-bs (make-hasheq))
-  (define d empty)
+  (define d '())
 
   (define ((start b))
-    (match-define (list l h r) b)
-    (match-define (list _ old-max-h _) (heap-min active-bs))
+    (define old-max-h (cadr (heap-min active-bs)))
     (heap-add! active-bs b)
-    (when (> h old-max-h)
-      (set! d (cons (list l h) d))))
+    (when (> (cadr b) old-max-h)
+      (set! d (cons (list (car b) (cadr b)) d))))
   (define ((end b))
-    (match-define (list l h r) b)
-    (match-define (list _ old-max-h _) (heap-min active-bs))
+    (define old-max-h (cadr (heap-min active-bs)))
     (hash-set! ended-bs b #t)
     (while (hash-ref ended-bs (heap-min active-bs) #f)
       (heap-remove-min! active-bs))
-    (match-define (list _ new-max-h _) (heap-min active-bs))
+    (define new-max-h (cadr (heap-min active-bs)))
     (unless (= old-max-h new-max-h)
-      (set! d (cons (list r new-max-h) d))))
+      (set! d (cons (list (caddr b) new-max-h) d))))
 
-  (define events (make-heap (lift <= car)))
+  (define events (make-heap (lift < car)))
   (for ([b (in-list bs)])
-    (match-define (list l h r) b)
-    (heap-add! events (cons l (start b)))
-    (heap-add! events (cons r (end b))))
-  (for ([e (in-heap events)]) ((cdr e)))
+    (heap-add! events (cons (car b) (start b)))
+    (heap-add! events (cons (caddr b) (end b))))
+  (for ([e (in-heap events)]) 
+    ((cdr e)))
   (reverse d))
 
 (module+ test
-  (require rackunit)
+  (require rackunit racket/list)
   (define in
     '((1 11 5) (2 6 7) (3 13 9) (12 7 16) (14 3 25) (19 18 22) (23 13 29) (24 4 28)))
   (define out
