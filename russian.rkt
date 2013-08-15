cb11a02ceaf2f1a798244a11f8db7c0034f1e1f0
Jay McCarthy <jay.mccarthy@gmail.com>
2013-08-15 14:00:01 -0600

    Automatic commit at Thu Aug 15 14:00:01 MDT 2013

--- russian.rkt
+++ russian.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 (require racket/file
+         racket/function
          net/url
          racket/string
          racket/match
@@ -76,56 +77,6 @@
 
 (struct word (freq url word english part) #:transparent)
 
-(define (parse/word word! w)
-  (match w
-    [(word f #f r e p)
-     (word!
-      (list f r "" e p))]
-    [(word f us r e p)
-     (define x (read-url/cache (string->url us)))
-
-     ;; xxx Verb conjugation: [verbs]
-     ;; xxx Proverbs and sayings: [sent]
-     ;; xxx Other forms of the word (declensions): [noun declensions]
-     ;; xxx Related words: [words]
-     ;; xxx Example sentences: [sent]
-
-     (define mp3s ((sxpath '(// (div (@ (equal? (id "wod_vocab")))) (script 3) *text*)) x))
-     (define mp3-us
-       (match (regexp-match #rx"soundFile: \"(.*?)\"," (apply string-append mp3s))
-         [(list _ mp3)
-          mp3]
-         [else
-          (unless (member f '(83 152 250 282 396 430 568 574))
-            (error 'russian "~v ~v ~v"
-                   us
-                   mp3s
-                   ((sxpath '(// (div (@ (equal? (id "wod_vocab")))))) x)))
-          #f]))
-
-     ;; xxx Idioms and set expressions: [words]
-     (define cx (rest (rest (first ((sxpath '(// (div (@ (equal? (class "col1")))))) x)))))
-     (let loop ([cx cx])
-       (unless (empty? cx)
-         (match (first cx)
-           [`(div (@ (id "wod_header")) . ,kind)
-            (match (string-trim (apply string-append (filter string? kind)))
-              [k
-               (error 'word! "~v" k)])]
-           [else
-            (loop (rest cx))])))
-
-     (word!
-      (list f r
-            (cond
-              [mp3-us
-               (define u (string->url mp3-us))
-               (cache-static-url! u)
-               (url->cache-path u)]
-              [else
-               ""])
-            e p))]))
-
 (define (parse/list us)
   (define x (read-url/cache (string->url us)))
   (define trs ((sxpath '(// (table (@ (equal? (class "topwords")))) tr)) x))
@@ -155,6 +106,151 @@
         (word (parse-number-s number-s) word-url-s russian english part))))
   ws)
 
+(define (parse/word word! sentence! w)
+  (match w
+    [(word f #f r e p)
+     (word!
+      (list f r "" e p #f))]
+    [(word f us r e p)
+     (define x (read-url/cache (string->url us)))
+
+     ;; xxx Verb conjugation: [verbs]
+     ;; xxx Related words: [words]
+
+     (define mp3s ((sxpath '(// (div (@ (equal? (id "wod_vocab")))) (script 3) *text*)) x))
+
+     (define (parse-mp3-js mp3s)
+       (match (regexp-match #rx"soundFile: \"(.*?)\"," (apply string-append mp3s))
+         [(list _ mp3)
+          mp3]
+         [_
+          #f]))
+
+     (define mp3-us
+       (or (parse-mp3-js mp3s)
+           (begin (unless (member f '(83 152 250 282 396 430 568 574))
+                    (error 'russian "~v ~v ~v"
+                           us
+                           mp3s
+                           ((sxpath '(// (div (@ (equal? (id "wod_vocab")))))) x)))
+                  #f)))
+
+     (define (grab-mp3 mp3-us)
+       (cond
+         [mp3-us
+          (define u (string->url mp3-us))
+          (cache-static-url! u)
+          (url->cache-path u)]
+         [else
+          ""]))
+
+     (define cx (rest (rest (first ((sxpath '(// (div (@ (equal? (class "col1")))))) x)))))
+     (let loop ([cx cx])
+       (unless (empty? cx)
+         (match (first cx)
+           [`(div (@ (id "wod_header")) . ,kind)
+            (match (string-trim (apply string-append (filter string? kind)))
+              ["Related words:"
+               (eprintf "Unwise to parse related: ~a\n" us)
+               (loop (rest cx))]
+              [(or "Verb conjugation:"
+                   "Other forms of the word (declensions):")
+               (eprintf "Hard to parse tables: ~a\n" us)
+               (loop (rest cx))]
+              ["Idioms and set expressions:"
+               (let sloop ([cx (rest cx)])
+                 (match (first cx)
+                   [`(ul . ,lis)
+                    (let sloop2 ([lis lis])
+                      (match lis
+                        [`((li (b ,r) ,e=) . ,more)
+                         (define e (string-trim (second (string-split e= "="))))
+                         (sentence! (list r "" e ""))
+                         (sloop2 more)]
+                        [`((li (b ,r) ,e=1 (b ,e=2) ,e=3 (br) (& nbsp) (span (@ (class "highlight_lit")) (& nbsp) "literal" (& nbsp)) " " (i ,lit)) . ,more)
+                         (define e (string-trim (second (string-split (string-append e=1 e=2 e=3) "="))))
+                         (sentence! (list r "" e lit))
+                         (sloop2 more)]
+                        [`((li (b ,r) ,e= (br) (& nbsp) (span (@ (class "highlight_lit")) (& nbsp) "literal" (& nbsp)) " " (i ,lit)) . ,more)
+                         (define e (string-trim (second (string-split e= "="))))
+                         (sentence! (list r "" e ""))
+                         (sloop2 more)]
+                        [`()
+                         (void)]
+                        [x
+                         (error 'idioms "~a ~v" us x)]))
+                    (loop (rest cx))]
+                   [else
+                    (sloop (rest cx))]))]
+              [(or "Proverbs and sayings:"
+                   "Example sentences:")
+               (let sloop ([cx (rest cx)])
+                 (match (first cx)
+                   [`(ul (@ (class "phrase_plain")) . ,lis)
+                    (define parse-rpart
+                      (match-lambda
+                       [(list (? string? s))
+                        (values s #f)]
+                       [(list _ ...
+                              (list 'script (list '@ (list 'type "text/javascript"))
+                                    (? string? js) ...)
+                              (? string? rs) ...)
+                        (values (string-trim (apply string-append rs))
+                                (parse-mp3-js js))]
+                       [x
+                        (error 'rpart "~a ~a ~v" us kind x)]))
+
+                    (let sloop2 ([lis lis])
+                      (match lis
+                        [`((li (@ (class "first"))) (li) . ,more)
+                         (sloop2 more)]
+                        [`((li (@ (class "first")) (span (@ (style "color: green; font-size: 10px;font-family:Arial,helvetica;")) "Click on " #\► " to listen to the examples."))
+                           . ,more)
+                         (sloop2 more)]
+                        [`((li (@ (class "first")) . ,rpart)
+                           (li ,e)
+                           (li (span (@ (class "highlight_lit")) (& nbsp) "literal" (& nbsp)) ,lit)
+                           . ,more)
+                         (define-values (r rmp3) (parse-rpart rpart))
+                         (sentence! (list r (grab-mp3 rmp3)
+                                          (string-trim e)
+                                          (string-trim lit)))
+                         (sloop2 more)]
+                        [`((li (@ (class "first")) . ,rpart)
+                           (li)
+                           (li (span (@ (class "highlight_lit")) (& nbsp) "literal" (& nbsp)) ,lit)
+                           . ,more)
+                         (define-values (r rmp3) (parse-rpart rpart))
+                         (sentence! (list r (grab-mp3 rmp3)
+                                          ""
+                                          (string-trim lit)))
+                         (sloop2 more)]
+                        [`((li (@ (class "first")) . ,rpart)
+                           (li ,e)
+                           . ,more)
+                         (define-values (r rmp3) (parse-rpart rpart))
+                         (sentence! (list r (grab-mp3 rmp3)
+                                          (string-trim e)
+                                          ""))
+                         (sloop2 more)]
+                        [`()
+                         (void)]
+                        [x
+                         (error 'sentences "~a ~v" us x)]))
+                    (loop (rest cx))]
+                   [else
+                    (sloop (rest cx))]))]
+              [k
+               (error 'word! "unknown kind ~v @ ~a" k us)])]
+           [else
+            (loop (rest cx))])))
+
+     (word!
+      (list f r
+            (grab-mp3 mp3-us)
+            e p
+            us))]))
+
 (define (write-csv l)
   (for ([r (in-list l)])
     (for ([e (in-list r)])
@@ -164,16 +260,21 @@
 
 (define word->csv
   (match-lambda
-   [(list freq r mp3-url e p)
-    (list (number->string freq)
-          r
+   [(list freq r mp3-url e p us)
+    (list r
           (match mp3-url
             [""
              ""]
             [else
              (format "[sound:~a]" mp3-url)])
           `(span (@) . ,e)
-          p)]))
+          p
+          (if freq
+            (number->string freq)
+            "")
+          (if us
+            `(a (@ (href ,us)) ,us)
+            ""))]))
 
 (define (snoc l x)
   (append l (list x)))
@@ -186,18 +287,23 @@
        ;; xxx 8 is wrong and needs a tr on line 225
        (parse/list (format "http://masterrussian.com/vocabulary/most_common_words_~a.htm" i)))))
 
-  (define word-db empty)
-  (define (word! l)
-    (set! word-db (snoc word-db (word->csv l))))
+  (define-syntax-rule (define-extend! extend! word-db word->csv)
+    (begin (define word-db empty)
+           (define (extend! l)
+             (set! word-db (snoc word-db (word->csv l))))))
+
+  (define-extend! word! word-db word->csv)
+  (define-extend! sentence! sentence-db identity)
 
   (for ([w (in-list ws)])
-    (parse/word word! w))
+    (parse/word word! sentence! w))
 
-
-  (with-output-to-file
-      (build-path *output-dir* "word.csv")
+  (with-output-to-file (build-path *output-dir* "word.csv")
     #:exists 'replace
     (λ () (write-csv word-db)))
+  (with-output-to-file (build-path *output-dir* "sentence.csv")
+    #:exists 'replace
+    (λ () (write-csv sentence-db)))
 
   (length word-db))
 
