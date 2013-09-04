c68a9b2593579d9f466f3258543b6e4a2afe60d6
Jay McCarthy <jay.mccarthy@gmail.com>
2013-09-04 08:00:01 -0600

    Automatic commit at Wed Sep  4 08:00:01 MDT 2013

--- sstruct2.rkt
+++ sstruct2.rkt
@@ -1,8 +1,35 @@
 #lang racket/base
+(require (for-syntax racket/base
+                     syntax/parse)
+         racket/list)
+
+(begin-for-syntax
+  (define-syntax-class formals
+    [pattern rest:id]
+    [pattern (mandatory:id . rest:formals)]
+    [pattern ()])
+  (define-syntax-class option))
+
+(define-syntax (sstruct stx)
+  (syntax-parse stx
+    [(_ (n:id . h:formals) o:option ...)
+     (syntax/loc stx
+       (begin
+         (define-values (n:type n:constructor n? n:accessor n:mutator)
+           (make-struct-type 'n #f 0 0 #f empty (current-inspector) #f empty #f 'n))))]))
 
 (module+ test
+  (require rackunit)
+
   (let ()
     (sstruct (posn x y))
     (define p0 (posn 1 2))
+    (check-equal? (posn-x p0) 1)
+    (check-equal? (posn-y p0) 2))
+
+  (let ()
+    (sstruct (posn x y)
+             #:accessor "posn.~a")
+    (define p0 (posn 1 2))
     (check-equal? (posn.x p0) 1)
-    (check-equal? (posn.x p0) 2)))
+    (check-equal? (posn.y p0) 2)))
