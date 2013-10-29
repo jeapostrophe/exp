3113dcd717fd77b29f570df95bcfc28c5e60bee1
Jay McCarthy <jay.mccarthy@gmail.com>
2013-10-29 10:00:01 -0600

    Automatic commit at Tue Oct 29 10:00:01 MDT 2013

--- turing-machines.rkt
+++ turing-machines.rkt
@@ -52,6 +52,17 @@
                                  (check-LR 'head-movement)))
                      ...)))))]))
 
+(define (truncate-trailing x l)
+  (let loop ([y empty] [m empty] [l l])
+    (cond
+      [(empty? l)
+       (reverse y)]
+      [(eq? x (first l))
+       (loop y (list* x m) (rest l))]
+      [else
+       (define ny (list* (first l) m))
+       (loop ny ny (rest l))])))
+
 (struct tape (blank before after))
 (define (tape-first t)
   (match-define (tape b before after) t)
@@ -65,6 +76,9 @@
     [(list) empty]))
 (define (tape-tser t)
   (reverse (tape-before t)))
+(define (tape->list t)
+  (append (reverse (truncate-trailing (tape-blank t) (tape-before t)))
+          (truncate-trailing (tape-blank t) (tape-after t))))
 
 (define (tape-move-left t)
   (match-define (tape b before after) t)
@@ -113,14 +127,16 @@
 
 (define (step-n tm s n
                 #:inform [inform-f void])
-  (inform-f s)
-  (cond
-    [(zero? n)
-     s]
-    [else
-     (define ns (step tm s))
-     (step-n tm ns (sub1 n)
-             #:inform inform-f)]))
+  (let loop ([i 0] [n n] [s s])
+    (inform-f i s)
+    (cond
+      [(zero? n)
+       s]
+      [else
+       (define ns (step tm s))
+       (if (eq? s ns)
+         s
+         (loop (add1 i) (sub1 n) ns))])))
 
 (define (run tm input steps
              #:inform [inform-f void])
@@ -136,8 +152,15 @@
      (tape (*tm-blank tm)
            empty
            input)))
-  (step-n tm initial-s steps
-          #:inform inform-f))
+  (define final-s
+    (step-n tm initial-s steps
+            #:inform inform-f))
+  (tape->list
+   (*state-tape final-s)))
+
+(define (run* tm input
+              #:inform [inform-f void])
+  (run tm input +inf.0 #:inform inform-f))
 
 (require racket/format
          racket/string)
@@ -146,15 +169,18 @@
     (apply max
            (map (compose string-length symbol->string)
                 (*tm-states tm))))
-  (λ (s)
+  (λ (i s)
     (match-define (*state st t) s)
     (displayln
      (~a (~a #:min-width max-st-len st) ": "
          (string-append* (map ~a (tape-tser t)))
          "[" (tape-first t) "]"
-         (string-append* (map ~a (tape-rest t)))))))
+         (string-append* (map ~a (tape-rest t)))
+         " {" (~a i) "}"))))
 
 (module+ test
+  (require rackunit)
+
   (define busy-beaver
     (tm #:Q (A B C HALT)
         #:G (0 1)
@@ -170,10 +196,11 @@
         [(C 0) (   B 1 L)]
         [(C 1) (HALT 1 R)]))
 
-  (run busy-beaver
-       empty
-       14
-       #:inform (make-display-state busy-beaver))
+  (check-equal?
+   (run* busy-beaver
+         '()
+         #:inform (make-display-state busy-beaver))
+   '(1 1 1 1 1 1))
 
   (define unary-addition
     (tm #:Q (consume-first-number
@@ -202,10 +229,11 @@
         [(seek-beginning _)
          (HALT _ R)]))
 
-  (run unary-addition
-       '(* * * * * / * * * * *)
-       24
-       #:inform (make-display-state unary-addition)))
+  (check-equal?
+   (run* unary-addition
+         '(* * * * * / * * * * *)
+         #:inform (make-display-state unary-addition))
+   '(* * * * * * * * * *)))
 
 (begin-for-syntax
   (define (syntax->slist s)
@@ -271,7 +299,7 @@
     (implicit-tm
      [consume-first-number
       [* (consume-first-number * R)]
-      [/ (consume-second-number * R)]]
+      [+ (consume-second-number * R)]]
      [consume-second-number
       [* (consume-second-number * R)]
       [_ (override-last-* _ L)]]
@@ -281,15 +309,148 @@
       [* (seek-beginning * L)]
       [_ (HALT _ R)]]))
 
-  (run implicit-unary-addition
-       '(* * * * * / * * * * *)
-       24
-       #:inform (make-display-state implicit-unary-addition))
+  (check-equal?
+   (run* implicit-unary-addition
+         '(* * * * * + * * * * *)
+         #:inform (make-display-state implicit-unary-addition))
+   '(* * * * * * * * * *))
 
-  (define implicit-binary-addition
-    (implicit-tm))
+  (define implicit-binary-add1
+    (implicit-tm
+     [find-end
+      [0 (find-end 0 R)]
+      [1 (find-end 1 R)]
+      [_ (zero-until-0 _ L)]]
+     [zero-until-0
+      [1 (zero-until-0 0 L)]
+      [0 (HALT 1 L)]]))
 
-  (run implicit-binary-addition
-       '($ 0 0 1 0 $ 0 0 1 1)
-       24
-       #:inform (make-display-state implicit-binary-addition)))
+  (check-equal?
+   (run* implicit-binary-add1
+         '(0 0 1 0)
+         #:inform (make-display-state implicit-binary-add1))
+   '(0 0 1 1))
+
+  (define implicit-binary-sub1
+    (implicit-tm
+     [ones-complement
+      [0 (ones-complement 1 R)]
+      [1 (ones-complement 0 R)]
+      [_ (add1:zero-until-0 _ L)]]
+     [add1:zero-until-0
+      [1 (add1:zero-until-0 0 L)]
+      [0 (add1:find-end 1 R)]]
+     [add1:find-end
+      [0 (add1:find-end 0 R)]
+      [1 (add1:find-end 1 R)]
+      [_ (ones-complementR _ L)]]
+     [ones-complementR
+      [0 (ones-complementR 1 L)]
+      [1 (ones-complementR 0 L)]
+      [_ (HALT _ R)]]))
+
+  (check-equal?
+   (run* implicit-binary-sub1
+         '(0 0 1 0)
+         #:inform (make-display-state implicit-binary-sub1))
+   '(0 0 0 1))
+
+  (define implicit-binary-add
+    (implicit-tm
+     [check-if-zero
+      [0 (check-if-zero 0 R)]
+      [1 (seek-left&sub1 1 L)]
+      [+ (seek-left&zero _ L)]]
+     [seek-left&sub1
+      [0 (seek-left&sub1 0 L)]
+      [1 (seek-left&sub1 1 L)]
+      [_ (sub1:ones-complement _ R)]]
+     [sub1:ones-complement
+      [0 (sub1:ones-complement 1 R)]
+      [1 (sub1:ones-complement 0 R)]
+      [+ (sub1:add1:zero-until-0 + L)]]
+     [sub1:add1:zero-until-0
+      [1 (sub1:add1:zero-until-0 0 L)]
+      [0 (sub1:add1:find-end 1 R)]]
+     [sub1:add1:find-end
+      [0 (sub1:add1:find-end 0 R)]
+      [1 (sub1:add1:find-end 1 R)]
+      [+ (sub1:ones-complementR + L)]]
+     [sub1:ones-complementR
+      [0 (sub1:ones-complementR 1 L)]
+      [1 (sub1:ones-complementR 0 L)]
+      [_ (seek-right&add1 _ R)]]
+     [seek-right&add1
+      [0 (seek-right&add1 0 R)]
+      [1 (seek-right&add1 1 R)]
+      [+ (add1:find-end + R)]]
+     [add1:find-end
+      [0 (add1:find-end 0 R)]
+      [1 (add1:find-end 1 R)]
+      [_ (add1:zero-until-0 _ L)]]
+     [add1:zero-until-0
+      [1 (add1:zero-until-0 0 L)]
+      [0 (seek-left&continue 1 L)]]
+     [seek-left&continue
+      [0 (seek-left&continue 0 L)]
+      [1 (seek-left&continue 1 L)]
+      [+ (seek-left&continue + L)]
+      [_ (check-if-zero _ R)]]
+     [seek-left&zero
+      [0 (seek-left&zero _ L)]
+      [_ (seek-start _ R)]]
+     [seek-start
+      [_ (seek-start _ R)]
+      [0 (move-right-once 0 L)]
+      [1 (move-right-once 1 L)]]
+     [move-right-once
+      [_ (HALT _ R)]]))
+
+  (check-equal?
+   (run* implicit-binary-add
+         '(0 0 1 0 + 0 0 1 1)
+         #:inform (make-display-state implicit-binary-add))
+   '(0 1 0 1))
+
+  (check-equal?
+   (run* implicit-binary-add
+         '(0 0 1 1 0 + 0 1 0 1 1)
+         #:inform (make-display-state implicit-binary-add))
+   '(1 0 0 0 1)))
+
+;; Rendering
+(require 2htdp/universe
+         2htdp/image)
+
+(define (next-state l)
+  (match l
+    [(cons x (and nl (cons y r))) nl]
+    [_ l]))
+(define (beside* l)
+  (cond
+    [(empty? l) empty-image]
+    [(cons? (cdr l)) (car l)]
+    [else
+     (apply beside/align "bottom" l)]))
+(define (render-cell s)
+  (text (~a s) 12 "black"))
+(define (draw-state l)
+  (define s (first l))
+
+  (match-define (*state st t) s)
+  (beside* (map render-cell (tape-tser t)))
+  (render-cell (tape-first t))
+  (beside* (map render-cell (tape-rest t)))
+
+  (empty-scene 500 500))
+
+(define (render tm input)
+  (define l empty)
+  (run* tm input #:inform (λ (i s) (set! l (cons s l))))
+  (big-bang (reverse l)
+            [on-draw draw-state]
+            [on-tick next-state]))
+
+(module+ test
+  (render implicit-binary-add
+          '(0 0 1 1 0 + 0 1 0 1 1)))
