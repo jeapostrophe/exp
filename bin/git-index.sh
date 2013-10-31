42caf199d9523ea4c4ce136dc58bcd4248f758d7
Jay McCarthy <jay.mccarthy@gmail.com>
2013-10-30 19:00:01 -0600

    Automatic commit at Wed Oct 30 19:00:01 MDT 2013

--- bin/git-index.sh
+++ bin/git-index.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/jay/Dev/scm/plt/racket/bin/racket -t /home/jay/Dev/scm/github.jeapostrophe/git-index/git-index.rkt
+/home/jay/Dev/scm/plt/racket/bin/racket -t /home/jay/Dev/scm/github.jeapostrophe/git-index/git-index.rkt -- $*
