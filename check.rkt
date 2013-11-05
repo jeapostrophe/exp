6db5de42622cd5a42a320bd11b5c04167de8b380
Jay McCarthy <jay.mccarthy@gmail.com>
2013-11-04 19:00:01 -0700

    Automatic commit at Mon Nov  4 19:00:01 MST 2013

--- check.rkt
+++ check.rkt
@@ -1,11 +1,21 @@
 #lang racket/base
 (require racket/match
+         racket/function
+         racket/list
          (prefix-in ru: rackunit)
+         syntax/parse/define
          (for-syntax racket/base
                      syntax/parse))
 
-(struct res:exn (x) #:transparent)
-(struct res:values (vs) #:transparent)
+(struct res:exn (x))
+(struct res:values (vs))
+(define (res-equal? x y)
+  (or (and (res:values? x) (res:values? y)
+           (equal? (res:values-vs x)
+                   (res:values-vs y)))
+      (and (res:exn? x) (res:exn? y)
+           (equal? (exn-message (res:exn-x x))
+                   (exn-message (res:exn-x y))))))
 
 (define-syntax-rule (->values e)
   (with-handlers ([exn? (λ (x) (res:exn x))])
@@ -13,31 +23,47 @@
       (λ vs (res:values vs)))))
 
 (define-syntax-rule (check-equal? x y)
-  (ru:check-equal? (->values x) (->values y)))
+  (ru:check res-equal? (->values x) (->values y)))
 (define-syntax-rule (check-not-equal? x y)
-  (ru:check-not-equal? (->values x) (->values y)))
+  (ru:check (negate res-equal?) (->values x) (->values y)))
 (define-syntax-rule (check-not-false x)
-  (ru:check-not-false (->values x)))
+  (ru:test-begin
+   (define xv (->values x))
+   (ru:check-pred res:values? xv)
+   (define len (length (res:values-vs xv)))
+   (if (= len 1)
+     (ru:check-not-false (first (res:values-vs xv)))
+     (ru:check-not-equal? len 1))))
 (define-syntax-rule (check-false x)
-  (ru:check-false (->values x)))
+  (ru:test-begin
+   (define xv (->values x))
+   (cond
+     [(res:values? xv)
+      (define len (length (res:values-vs xv)))
+      (if (= len 1)
+        (ru:check-false (first (res:values-vs xv)))
+        (ru:check-not-equal? len 1))]
+     [else
+      (ru:check-pred res:exn? xv)])))
 
 (define (exn-match x b)
   (match b
-    [(? string?)
-     (exn-match x (regexp (regexp-quote b)))]
-    [(? regexp?)
-     (regexp-match b (exn-message x))]
-    [(? procedure?)
-     (b x)]))
+    [(? string? s)
+     (exn-match x (regexp (regexp-quote s)))]
+    [(? regexp? r)
+     (regexp-match r (exn-message x))]
+    [(? procedure? okay?)
+     (okay? x)]))
 
 (define-syntax-rule (check-exn a b)
   (ru:check-exn
    (λ (x) (exn-match x b))
    (λ () a)))
 (define-syntax-rule (check-not-exn a b)
-  (ru:check-exn
-   (λ (x) (not (exn-match x b)))
-   (λ () a)))
+  (ru:test-begin
+   (define av (->values a))
+   (when (res:exn? av)
+     (ru:check-false (exn-match (res:exn-x av) b)))))
 
 (begin-for-syntax
   (define-splicing-syntax-class test
@@ -71,37 +97,41 @@
              #:attr unit #'c.unit
              #:attr fail-unit #'c.fail-unit]))
 
-(define-syntax (chk stx)
-  (syntax-parse stx
-    [(_ e:test ...)
-     (syntax/loc stx
-       (begin e.unit ...))]))
+(define-simple-macro (chk e:test ...)
+  (begin e.unit ...))
 
-(define-syntax-rule (checks . b)
-  (module+ test (chk . b)))
+(module+ test
+  (chk
+   1 1
+   #:f 1 0
+   #:f #:f #:f 1 0
+   #:f #:f 1 1
+   #:f (/ 1 0) +inf.0
+   (/ 1 0) (/ 1 0)
+   #:f (error 'xxx "a") (error 'xxx "b")
 
-(checks
- 1 1
- #:f 1 0
- #:f #:f 1 1
- #:f (/ 1 0) +inf.0
- (/ 1 0) (/ 1 0)
+   #:f #:t (/ 1 0)
+   #:t (values 0 1)
+   #:t (values #f 1)
+   #:f #:t (values #f 1)
 
- 1 1
- 2 2
- #:exn (/ 1 0) "division"
- #:exn (/ 1 0) #rx"di.ision"
- ;;#:fail #:exn (/ 1 0) "diblision"
+   1 1
+   2 2
+   #:exn (/ 1 0) "division"
+   #:exn (/ 1 0) #rx"di.ision"
+   #:f #:exn (/ 1 1) "division"
+   #:f #:exn (/ 1 0) "diblision"
 
- #:t (chk 1)
- #:t 1
- #:f #f
- #:f #:t #f
- 1 1
- #:t 1
- #:f 2 3
+   #:t (chk 1)
+   #:t 1
+   #:f #f
+   #:f #:t #f
+   1 1
+   #:t 1
+   #:f 2 3
 
- (values 1 2) (values 1 2)
- #:f (values 1 2) (values 2 3)
- #:f (values 1 2) 3
- (quotient/remainder 10 3) (values 3 1))
+   (values 1 2) (values 1 2)
+   #:f (values 1 2) (values 2 3)
+   #:f (values 1 2) 3
+   #:f 3 (values 1 2)
+   (quotient/remainder 10 3) (values 3 1)))
