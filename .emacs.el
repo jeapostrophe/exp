6bfa45d19b4c856950751f0617e121579cf07b84
Jay McCarthy <jay.mccarthy@gmail.com>
2013-11-07 11:00:01 -0700

    Automatic commit at Thu Nov  7 11:00:01 MST 2013

--- .emacs.el
+++ .emacs.el
@@ -413,7 +413,7 @@ given a prefix arg."
             ("cg" . "cgc -noentry")
             ("glsl" . "cgc -noentry -oglsl")
             ;; XXX rkt to rk switch
-            ("rkt" . "rkt")
+            ("rkt" . "rk")
             ("txt" . "checkwriting")
             ("scrbl" . "scribble --pdf")
             ("dot" . "dot -Tpdf -O")
