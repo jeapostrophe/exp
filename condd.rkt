8640286497fe966520c30445cb2cc5fedb344120
Jay McCarthy <jay.mccarthy@gmail.com>
2013-11-12 06:00:01 -0700

    Automatic commit at Tue Nov 12 06:00:01 MST 2013

--- condd.rkt
+++ condd.rkt
@@ -1,6 +1,8 @@
 #lang racket/base
 (require (for-syntax racket/base
                      syntax/parse))
+(module+ test
+  (require rackunit/chk))
 
 (define-syntax (condd stx)
   (syntax-parse stx
@@ -18,29 +20,79 @@
          (let () . b)
          (condd . more))]))
 
-(condd
- [else (void)])
+(module+ test
+  (chk
+   (condd
+    [else (void)])
+   (void)
 
-(condd
- [else 1])
+   (condd
+    [else 1])
+   1
 
-(condd
- (define first 1)
- [else (void)])
+   (condd
+    (define first 1)
+    [else (void)])
+   (void)
 
-(condd
- [#t 1])
+   (condd
+    [#t 1])
+   1
 
-(condd
- (define first 1)
- [(= first 2)
-  (printf "2\n")]
- (define first 2)
- [(= first 2)
-  (printf "2 again\n")]
- [else
-  (printf "Nope\n")])
+   (condd
+    (define first 1)
+    [(= first 2)
+     "2\n"]
+    (define first 2)
+    [(= first 2)
+     "2 again\n"]
+    [else
+     "Nope\n"])
+   "2 again\n"
 
-(condd
- [#f 1]
- [#f 2])
+   #:exn
+   (condd
+    [#f 1]
+    [#f 2])
+   "Missing else"))
+
+(begin-for-syntax
+  (define-splicing-syntax-class switch-clause
+    #:attributes (code-gen)
+    (pattern (~seq #:cond [t:expr e:expr ...+])
+             #:attr code-gen
+             (λ (k)
+               (quasisyntax/loc #'t
+                 (if t (let () e ...) #,k))))))
+
+(define-syntax (switch stx)
+  (syntax-parse stx
+    [(_)
+     (syntax/loc stx
+       (error 'switch "Fell through without else clause"))]
+    [(_ #:else e:expr ...+)
+     (syntax/loc stx
+       (begin e ...))]
+    [(_ (~and (~seq x ...)
+              (~not (~seq x:switch-clause ...)))
+        sc:switch-clause . tail)
+     (quasisyntax/loc stx
+       (let ()
+         x ...
+         #,((attribute sc.code-gen)
+            (syntax/loc stx
+              (switch . tail)))))]))
+
+(module+ test
+  (chk #:exn (switch) "Fell through"
+       (switch #:else 1) 1
+       (switch #:else 1 2) 2
+       (switch #:cond [1 2]) 2
+       (switch #:cond [#f 1] #:else 2) 2
+       (switch #:cond [1 (define x 2) x]) 2
+       (switch #:cond [#f 1] #:cond [2 3] #:else 4) 3
+       (switch (define one 1)
+               #:cond [#f 1]
+               (define two 2)
+               #:cond [two (+ one one one)]
+               #:else 4) 3))
