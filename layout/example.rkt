5bd7b41cee642d3f6a499f56e46499fadae88254
Jay McCarthy <jay.mccarthy@gmail.com>
2013-11-21 19:00:01 -0700

    Automatic commit at Thu Nov 21 19:00:01 MST 2013

--- layout/example.rkt
+++ layout/example.rkt
@@ -1,7 +1,9 @@
 #lang racket/base
 (require (for-syntax racket/base
                      racket/syntax
-                     syntax/parse))
+                     syntax/parse)
+         racket/vector
+         racket/list)
 (module+ test
   (require rackunit/chk))
 
@@ -13,7 +15,7 @@
   (define-values (ty make pred acc mut)
     (make-struct-type name #f 1 0 #f props inspector #f (list 0) #f name))
   (values
-   cons
+   make
    pred
    (λ (x) (acc x 0))))
 
@@ -22,9 +24,18 @@
     #:attributes (cpat kw)
     [pattern n:keyword
              #:attr kw #'n
+             #:attr cpat (generate-temporary #'n)]
+    ;; xxx
+    [pattern [ct:expr n:keyword]
+             #:attr kw #'n
+             #:attr cpat (generate-temporary #'n)]
+    ;; xxx
+    [pattern [n:keyword #:mutable]
+             #:attr kw #'n
              #:attr cpat (generate-temporary #'n)])
 
   (define-syntax-class struct-option
+    ;; xxx
     [pattern #:mutable]))
 
 (define-syntax (struct stx)
@@ -33,23 +44,59 @@
      (with-syntax ([(f.idx ...)
                     (for/list ([i (in-naturals)]
                                [f (in-stx-list #'(f ...))])
-                      i)])
+                      i)]
+                   [field-count
+                    (length (syntax->list #'(f ...)))])
        (syntax/loc stx
          (begin
-           (define-values (seal seal? unseal) (make-seal))
+           (define-values (seal seal? unseal)
+             ;; xxx parent
+             ;; xxx pass props
+             ;; xxx pass inspector
+             (make-seal 'n empty (current-inspector)))
            (define-syntax (n n-stx)
+             (define-syntax-class n-kw
+               (pattern f.kw
+                        #:attr idx #'f.idx)
+               ...)
              (syntax-parse n-stx
-               [(_ ne:expr f.kw)
+               [(_ #:? ne:expr)
                 (syntax/loc n-stx
-                  (vector-ref (unseal ne) f.idx))]
-               ...
-               [(_ ne:expr f.kw nf:expr #:!)
+                  (seal? ne))]
+               [(_ ne:expr nf:n-kw (... ...))
                 (syntax/loc n-stx
-                  (vector-set! (unseal ne) f.idx nf))]
-               ...
-               [(_ (~seq (~optional f.kw) f.cpat) ...)
+                  (let ([une (unseal ne)])
+                    ;; xxx have nf have a reference attr that knows
+                    ;; the offset
+                    (values (vector-ref une nf.idx)
+                            (... ...))))]
+               ;; xxx implement mutability option
+               ;; xxx hide ordering?
+               [(_ ne:expr (~seq nf:n-kw nf-e:expr) (... ...) #:!)
                 (syntax/loc n-stx
-                  (seal (vector f.cpat ...)))])))))]))
+                  (let ([une (unseal ne)])
+                    ;; xxx here too
+                    (vector-set! une nf.idx nf-e)
+                    (... ...)))]
+               ;; xxx hide ordering?
+               [(_ ne:expr (~seq nf:n-kw nf-e:expr) (... ...))
+                (syntax/loc n-stx
+                  (let* ([une (unseal ne)]
+                         [unec (vector-copy une)])
+                    ;; xxx and here
+                    (vector-set! unec nf.idx nf-e)
+                    (... ...)
+                    (seal unec)))]
+               ;; xxx allow any order
+               ;; xxx hide ordering?
+               ;; xxx allow optional fields?
+               [(_ (~seq (~optional f.kw) (~var f.cpat expr)) ...)
+                (syntax/loc n-stx
+                  (let ([une (make-vector field-count)])
+                    ;; xxx and here
+                    (vector-set! une f.idx f.cpat)
+                    ...
+                    (seal une)))])))))]))
 
 (module+ test
   (define-syntax-rule (check-kons kons)
@@ -57,13 +104,21 @@
       (define k (kons 1 2))
       (chk #:t k
            #:t (kons #:? k)
-           #:f (kons #:? 1)
-           #:f (kons #:? 2)
+           #:f #:t (kons #:? 1)
+           #:f #:t (kons #:? 2)
            (kons (kons #:car 1 #:cdr 2) #:car) 1
            ;; xxx waiting for ~list-any-order
            ;; (kons (kons #:cdr 2 #:car 1) #:car) 1
            (kons k #:car) 1
+           (kons k #:cdr) 2
+           (kons k #:car #:cdr) (values 1 2)
+           (kons k #:cdr #:car) (values 2 1)
+           (kons k #:cdr #:car #:cdr) (values 2 1 2)
+           ;; xxx only specific order allowed
            ;; (kons #:cdr k) 2
+           (kons (kons k #:cdr 3) #:car) 1
+           (kons (kons k #:cdr 3 #:car 2) #:cdr #:car) (values 3 2)
+           (kons (kons k #:car 2 #:cdr 3) #:cdr #:car) (values 3 2)
            (kons (kons k #:cdr 3) #:cdr) 3))))
 
 (module+ test
