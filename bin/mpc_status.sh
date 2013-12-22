592c5d7cc47b4fc69c7b675f901ebd38938491e5
Jay McCarthy <jay.mccarthy@gmail.com>
2013-12-22 10:00:01 -0700

    Automatic commit at Sun Dec 22 10:00:01 MST 2013

--- bin/mpc_status.sh
+++ bin/mpc_status.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/sh
 
+if [ "${1}" != "" ] ; then
+    mpc ${1}
+fi
+
 if (mpc | grep paused) &> /dev/null ; then
  COLOR="#dc322f"
 else
  COLOR="#268bd2"
 fi
 
-echo -n "<fc=${COLOR}>"
-echo -n $(mpc -f '%artist%, %album%-%track%' current)
-echo "</fc>"
+CUR=$(mpc -f '%artist%, %album%-%track%' current)
+
+PIPE=~jay/.mpc-pipe
+if [ ! -e ${PIPE} ] ; then
+    mkfifo ${PIPE}
+fi
+echo "<fc=${COLOR}>${CUR}</fc>" > ${PIPE}
