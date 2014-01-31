fa7b4b70d299fec8045421490eb77e923e65379c
Jay McCarthy <jay.mccarthy@gmail.com>
2014-01-31 06:00:01 -0700

    Automatic commit at Fri Jan 31 06:00:01 MST 2014

--- bin/git-index.sh
+++ bin/git-index.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+/home/jay/Dev/scm/plt/racket/bin/raco make /home/jay/Dev/scm/github.jeapostrophe/git-index/git-index.rkt
 /home/jay/Dev/scm/plt/racket/bin/racket -t /home/jay/Dev/scm/github.jeapostrophe/git-index/git-index.rkt -- $*
