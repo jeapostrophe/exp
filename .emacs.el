0befea0d86e3f970af229e32e53a18d5bfba933a
Jay McCarthy <jay.mccarthy@gmail.com>
2014-06-18 12:00:01 -0600

    Automatic commit at Wed Jun 18 12:00:01 MDT 2014

--- .emacs.el
+++ .emacs.el
@@ -212,7 +212,8 @@
              (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
              ;; Edit files in dired with "e", which previously did what "RET" did
              (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)))
-(setq dired-listing-switches "-alh")
+(setq dired-listing-switches "-alho")
+(setq dired-use-ls-dired nil)
 ;; xxx change dired-find-alternate-file
 
 ;;;;; emacs-lisp-mode
@@ -1208,7 +1209,6 @@ given a prefix arg."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(dired-listing-switches "-alhoD")
  '(doc-view-continuous t)
  '(ibuffer-default-sorting-mode (quote filename/process))
  '(ibuffer-display-summary nil)
