b31482b8c4ffb8c6adc5a91ccefbb91df10dccd2
Jay McCarthy <jay.mccarthy@gmail.com>
2014-10-08 13:00:00 -0400

    Automatic commit at Wed Oct  8 13:00:00 EDT 2014

---
+++ isort-cost.rkt
@@ -0,0 +1,48 @@
+#lang racket
+(require math/base
+         plot)
+
+(define (isort-cost n is)
+  (cond
+   [(zero? n)
+    0]
+   [else
+    (if (first is)
+        (+ (sub1 n)
+           (isort-cost (sub1 n) (rest is)))
+        (+ 1
+           (isort-cost (sub1 n) (rest is))))]))
+
+(define (average l)
+  (if (empty? l)
+      0
+      (/ (sum l) (length l))))
+
+(define (list-with-m-trues-and-n-elements n m)
+  (build-list n (λ (x) (< x m))))
+
+(define (perms-with-m-trues-and-n-elements n m)
+  (remove-duplicates (permutations (list-with-m-trues-and-n-elements n m))))
+
+(module+ test
+  (remove-duplicates (permutations (list-with-m-trues-and-n-elements 5 3))))
+
+(module+ main
+  (plot-new-window? #t)
+  (parameterize ([plot-title  "Insertion Sort Cost"]
+                 [plot-x-label "n"]
+                 [plot-y-label "i"]
+                 [plot-z-label "cost"])
+    (plot3d
+     (contour-intervals3d (λ (n i)
+                            (argmax (λ (x) x)
+                             (map (λ (p)
+                                    (isort-cost (floor n) p))
+                                  (perms-with-m-trues-and-n-elements (floor n) (floor (min n i))))))
+                          0 8
+                          0 8))
+    (plot3d
+     (contour-intervals3d (λ (n i)
+                            (* n n))
+                          0 8
+                          0 8))))
