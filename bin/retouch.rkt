a0238fb21c44ed1878bd2203a73fa3be4b0acd57
Jay McCarthy <jay.mccarthy@gmail.com>
2014-12-30 09:00:01 -0500

    Automatic commit at Tue Dec 30 09:00:01 EST 2014

---
+++ bin/retouch.rkt
@@ -0,0 +1,19 @@
+#lang racket/base
+
+(define (retouch! ps)
+  (for/fold ([new-mtime #f])
+            ([p (in-list ps)])
+    (define old-mtime (file-or-directory-modify-seconds p))
+    (cond
+     [(and new-mtime (< old-mtime new-mtime))
+      (printf "retouch ~v ~a\n" p new-mtime)
+      (file-or-directory-modify-seconds p new-mtime)
+      (+ 1 new-mtime)]
+     [else
+      (+ 1 old-mtime)])))
+
+(module+ main
+  (require racket/cmdline)
+  (command-line #:program "retouch"
+                #:args files
+                (retouch! files)))
