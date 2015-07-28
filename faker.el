6b16e5b1b1c49f97ad31f560e05b0466410e256c
Jay McCarthy <jay.mccarthy@gmail.com>
2015-07-27 18:00:01 -0600

    Automatic commit at Mon Jul 27 18:00:01 MDT 2015

--- faker.el
+++ faker.el
@@ -1,11 +1,23 @@
 (defun faker-start ()
   "start faking the writing of a file"
   (interactive)
-  (let ((fn (read-file-name "Real file: ")))
+  (let* ((fn (read-file-name "Real file: "))
+         (bufn (generate-new-buffer-name " faker"))
+         (buf (get-buffer-create bufn)))
     (message "faking with fn")
-    (faker-loop fn)))
+    (with-current-buffer buf
+      (insert-file-contents fn))
+    (faker-loop buf)))
 
-(defun faker-loop (fn)
+(defun faker-loop (buf)
   (message "faker loop")
-  (read-event))
+  (message (read-event))
+  (message (read-event))
+  (message (read-event))
+  (message (read-event))
+  (message (read-event)))
+
+
+
+
 
