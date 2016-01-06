5acccb457b939777a39b4ba867464632a54c6838
Jay McCarthy <jay.mccarthy@gmail.com>
2016-01-06 17:00:01 -0500

    Automatic commit at Wed Jan  6 17:00:01 EST 2016

--- structs/cstruct.rkt
+++ structs/cstruct.rkt
@@ -5,7 +5,8 @@
 
 (define-cstruct _cs
   ([x _double]
-   [y _double]))
+   [y _double])
+  #:define-unsafe)
 
 (define N 180000)
 (define (test-fun)
@@ -16,13 +17,9 @@
          (fl+ (cs-x it)
               (cs-y it)))))
 
-;; XXX These could be exposed by define-cstruct but aren't
-;; (alternatively, it could expose cs-x-offset which would be useful
-;; for other reasons)
-(define (unsafe-cs-x it)
-  (ptr-ref it _double 'abs 0))
-(define (unsafe-cs-y it)
-  (ptr-ref it _double 'abs 8))
+cs-x-offset
+cs-y-offset
+
 (define (test-unsafe-fun)
   (define it (make-cs 1.0 2.0))
   (for/fold ([sum 0.0])
