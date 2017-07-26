9396d3bbdb36b9a5edc8a3e78c8c1ee5aefef53f
Jay McCarthy <jay.mccarthy@gmail.com>
2017-07-26 16:00:00 -0400

    Automatic commit at Wed Jul 26 16:00:00 EDT 2017

--- ranking/cost.rkt
+++ ranking/cost.rkt
@@ -12,8 +12,10 @@
 (module+ main
   (define path "/home/jay/Dev/scm/github.jeapostrophe/home/etc/games.org")
   (match-define (list games meta) (with-input-from-file path read-org))
+  (define k 0)
   (for ([g (in-list (node-children games))])
     (define p (node-props g))
     (when (equal? "Y" (hash-ref p "Owned" #f))
       (unless (hash-ref p "Price" #f)
-        (printf "~a\n" (node-label g))))))
+        (set! k (add1 k))
+        (printf "~a. ~a\n" k (node-label g))))))
