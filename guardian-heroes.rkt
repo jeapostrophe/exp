712c582a3a24bef99613c6db3006c98f62d278b2
Jay McCarthy <jay.mccarthy@gmail.com>
2017-12-10 20:00:01 -0500

    Automatic commit at Sun Dec 10 20:00:01 EST 2017

--- guardian-heroes.rkt
+++ guardian-heroes.rkt
@@ -1,51 +1,93 @@
 #lang racket/base
 (require graph
+         racket/list
+         racket/string
+         racket/match
+         racket/set
          racket/file
          racket/format
          racket/system)
 
 (define STAGES
-  (unweighted-graph/adj
-   '([1 2]
-     [2 4 5 3]
-     [3 6 8]
-     [4 12 7]
-     [5 11 10 9]
-     [6 11 10 9]
-     [7 11 10 9]
-     [8 13 14 15 16 17]
-     [9 13 14 15 16 17]
-     [10 13 14 15 16 17]
-     [11 13 14 15 16 17]
-     [12 13 14 15 16 17]
-     [13 18 19 20]
-     [14 18 19 20]
-     [15 19 20 21 22]
-     [16 21 22 23]
-     [17 21 22 23]
-     [18 24 25]
-     [19 24 25 26]
-     [20 24 25 26]
-     [21 26 27 28]
-     [22 26 27 28]
-     [23 27 28]
-     [24 end]
-     [25 end]
-     [26 29]
-     [27 30]
-     [28 end]
-     [29 end]
-     [30 end]
-     [end])))
+  '([start 1]
+    [1 2]
+    [2 4 5 3]
+    [3 6 8]
+    [4 12 7]
+    [5 11 10 9]
+    [6 11 10 9]
+    [7 11 10 9]
+    [8 13 14 15 16 17]
+    [9 13 14 15 16 17]
+    [10 13 14 15 16 17]
+    [11 13 14 15 16 17]
+    [12 13 14 15 16 17]
+    [13 18 19 20]
+    [14 18 19 20]
+    [15 19 20 21 22]
+    [16 21 22 23]
+    [17 21 22 23]
+    [18 24 25]
+    [19 24 25 26]
+    [20 24 25 26]
+    [21 26 27 28]
+    [22 26 27 28]
+    [23 27 28]
+    [24 end]
+    [25 end]
+    [26 29]
+    [27 30]
+    [28 end]
+    [29 end]
+    [30 end]
+    [end]))
 
-(define (render-graphviz dot png)
+(define (chase prev-ht n)
+  (define next (hash-ref prev-ht n #f))
+  (if next
+    (cons n (chase prev-ht next))
+    (list n)))
+
+(define (go! seen?)
+  (define g (weighted-graph/directed '()))
+  (define all-seen?
+    (for/fold ([all-seen? #t]) ([edge (in-list STAGES)])
+      (match-define (cons u ns) edge)
+      (for ([v (in-list ns)])
+        (add-directed-edge!
+         g u v
+         (if (set-member? seen? v) 0 -1)))
+      (and all-seen? (set-member? seen? u))))
+  (cond
+    [all-seen?
+     empty]
+    [else
+     (define-values (dist-ht prev-ht)
+       (dag-shortest-paths g 'start))
+     (define pth (reverse (chase prev-ht 'end)))
+     (cons pth
+           (go! (set-union seen? (list->seteq pth))))]))
+
+(define (render-graphviz/dot dot png)
   (system (~a "dot "dot" -Tpng -o "png)))
-(define (render-graph g png)
+(define (render-graphviz dot-content png)
   (define tmp (make-temporary-file))
-  (display-to-file (graphviz g) tmp #:exists 'replace)
-  (render-graphviz tmp png)
+  (displayln dot-content)
+  (display-to-file dot-content tmp #:exists 'replace)
+  (render-graphviz/dot tmp png)
   (delete-file tmp))
 
+(define (render-graph g png)
+  (render-graphviz (graphviz g) png))
+
 (module+ main
-  (render-graph STAGES "STAGES.png")
-  (render-graph (unweighted-graph/directed (mst-prim STAGES 1)) "MST.png"))
+  (require racket/pretty)
+  (render-graph (unweighted-graph/adj STAGES) "STAGES.png")
+
+  (define paths (go! (seteq)))
+  (render-graphviz
+   (string-join
+    (list
+     "graph {"
+     "}"))
+   "PATHS.png"))
