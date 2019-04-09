dd4211cd9ab00d58449fa89ed98c5fc2d0b3ab28
Jay McCarthy <jay.mccarthy@gmail.com>
2019-04-09 12:25:51 -0400

    Automatic commit at Tue Apr  9 12:25:51 EDT 2019

--- .emacs.el
+++ .emacs.el
@@ -278,6 +278,14 @@
          (proof-next-error))
         (t
          (proof-prf))))
+(defun je/normal-quotes (beg end)
+  "Replace 'smart quotes' in buffer or region with ascii quotes."
+  (interactive "r")
+  (format-replace-strings '(("\x201C" . "\"")
+                            ("\x201D" . "\"")
+                            ("\x2018" . "'")
+                            ("\x2019" . "'"))
+                          nil beg end))
 
 (defun je/run-current-file ()
   "Execute or compile the current file."
